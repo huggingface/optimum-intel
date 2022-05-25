567d4742af9448f56bd1d6ce29d77c94db8e50e0
Ella Charlaix <ella@huggingface.co>
2022-05-25 20:10:21 +0200

    Setup package

---
+++ README.md
@@ -0,0 +1,24 @@
+# Optimum Intel
+
+🤗 Optimum Intel is the interface between the 🤗 Transformers library and the different tools and libraries provided by Intel to accelerate end-to-end pipelines on Intel architectures.
+
+## Install
+To install the latest release of this package:
+
+`pip install optimum[intel]`
+
+Optimum Intel is a fast-moving project, and you may want to install from source.
+
+`pip install git+https://github.com/huggingface/optimum-intel.git`
+
+
+## Running the examples
+
+There are a number of examples provided in the `examples` directory.
+
+Please install the requirements for every example:
+
+```
+cd <example-folder>
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,60 @@
+import re
+
+from setuptools import find_namespace_packages, setup
+
+
+# Ensure we match the version set in optimum/intel/version.py
+try:
+    filepath = "optimum/intel/version.py"
+    with open(filepath) as version_file:
+        (__version__,) = re.findall('__version__ = "(.*)"', version_file.read())
+except Exception as error:
+    assert False, "Error: Could not open '%s' due %s\n" % (filepath, error)
+
+INSTALL_REQUIRES = [
+    "optimum",
+    "transformers",
+    "datasets",
+    "torch",
+    "sentencepiece",
+    "scipy",
+    "neural_compressor",
+]
+
+TESTS_REQUIRE = ["pytest", "parameterized"]
+
+EXTRAS_REQUIRE = {
+    "tests": TESTS_REQUIRE,
+}
+
+setup(
+    name="optimum-intel",
+    version=__version__,
+    description="Optimum Library is an extension of the Hugging Face Transformers library, providing a framework to "
+    "integrate third-party libraries from Hardware Partners and interface with their specific "
+    "functionality.",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    keywords="transformers, quantization, pruning, knowledge distillation, optimization, training",
+    url="https://www.intel.com",
+    author="HuggingFace Inc. Special Ops Team",
+    author_email="hardware@huggingface.co",
+    license="Apache",
+    packages=find_namespace_packages(include=["optimum*"]),
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRE,
+    include_package_data=True,
+    zip_safe=False,
+)
