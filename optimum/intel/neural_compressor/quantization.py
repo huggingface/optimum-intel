b21477b4211baeb80649285a458a233de03f8639
Ella Charlaix <80481427+echarlaix@users.noreply.github.com>
2022-07-27 14:42:05 +0200

    Fix quantized embedding expecting contiguous inputs (#12)
    * Fix quantized embedding expecting contiguous inputs
    
    * Fix style

--- optimum/intel/neural_compressor/quantization.py
+++ optimum/intel/neural_compressor/quantization.py
@@ -39,6 +39,7 @@ from transformers.utils.versions import require_version
 
 import neural_compressor
 from neural_compressor.adaptor.pytorch import PyTorch_FXAdaptor, _cfg_to_qconfig, _propagate_qconfig
+from neural_compressor.adaptor.torch_utils.util import get_embedding_contiguous
 from neural_compressor.conf.config import Quantization_Conf
 from neural_compressor.experimental import Quantization, common
 from neural_compressor.utils.pytorch import _load_int8_orchestration
@@ -318,6 +319,8 @@ class IncQuantizedModel:
         q_model = apply_quantization_from_config(inc_config, model)
 
         q_model.load_state_dict(state_dict, strict=False)
+
+        get_embedding_contiguous(q_model)
 
         return q_model
 
