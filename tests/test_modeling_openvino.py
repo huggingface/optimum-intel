c35dd4fd39bf0c9ba20886f10b1b66df757a159a
Ella Charlaix <ella@huggingface.co>
2022-08-03 17:42:55 +0200

    Add test

---
+++ tests/test_modeling_openvino.py
@@ -0,0 +1,159 @@
+#  Copyright 2021 The HuggingFace Team. All rights reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import gc
+import unittest
+
+import torch
+from transformers import (
+    AutoModelForQuestionAnswering,
+    AutoModelForSequenceClassification,
+    AutoModelForTokenClassification,
+    AutoTokenizer,
+    PretrainedConfig,
+    pipeline,
+    set_seed,
+)
+
+from optimum.intel.openvino.modeling import (
+    OVModelForQuestionAnswering,
+    OVModelForSequenceClassification,
+    OVModelForTokenClassification,
+)
+from parameterized import parameterized
+
+
+MODEL_NAMES = {
+    "distilbert": "hf-internal-testing/tiny-random-distilbert",
+    "bert": "hf-internal-testing/tiny-random-bert",
+    "roberta": "hf-internal-testing/tiny-random-roberta",
+}
+
+SEED = 42
+
+
+class OVModelForSequenceClassificationIntegrationTest(unittest.TestCase):
+    SUPPORTED_ARCHITECTURES = (
+        "distilbert",
+        "bert",
+        "roberta",
+    )
+
+    @parameterized.expand(SUPPORTED_ARCHITECTURES)
+    def test_compare_to_transformers(self, model_arch):
+        model_id = MODEL_NAMES[model_arch]
+        set_seed(SEED)
+        ov_model = OVModelForSequenceClassification.from_pretrained(model_id, from_transformers=True)
+        self.assertIsInstance(ov_model.config, PretrainedConfig)
+        transformers_model = AutoModelForSequenceClassification.from_pretrained(model_id)
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        tokens = tokenizer("This is a sample input", return_tensors="pt")
+        ov_outputs = ov_model(**tokens)
+        self.assertTrue("logits" in ov_outputs)
+        self.assertIsInstance(ov_outputs.logits, torch.Tensor)
+        with torch.no_grad():
+            transformers_outputs = transformers_model(**tokens)
+        self.assertTrue(torch.allclose(ov_outputs.logits, transformers_outputs.logits, atol=1e-4))
+        gc.collect()
+
+    @parameterized.expand(SUPPORTED_ARCHITECTURES)
+    def test_pipeline(self, model_arch):
+        model_id = MODEL_NAMES[model_arch]
+        model = OVModelForSequenceClassification.from_pretrained(model_id, from_transformers=True)
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        pipe = pipeline("text-classification", model=model, tokenizer=tokenizer)
+        outputs = pipe("This restaurant is awesome")
+
+        self.assertEqual(pipe.device, model.device)
+        self.assertGreaterEqual(outputs[0]["score"], 0.0)
+        self.assertIsInstance(outputs[0]["label"], str)
+        gc.collect()
+
+
+class OVModelForQuestionAnsweringIntegrationTest(unittest.TestCase):
+    SUPPORTED_ARCHITECTURES = (
+        "distilbert",
+        "bert",
+        "roberta",
+    )
+
+    @parameterized.expand(SUPPORTED_ARCHITECTURES)
+    def test_compare_to_transformers(self, model_arch):
+        model_id = MODEL_NAMES[model_arch]
+        set_seed(SEED)
+        ov_model = OVModelForQuestionAnswering.from_pretrained(model_id, from_transformers=True)
+        self.assertIsInstance(ov_model.config, PretrainedConfig)
+        transformers_model = AutoModelForQuestionAnswering.from_pretrained(model_id)
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        tokens = tokenizer("This is a sample input", return_tensors="pt")
+        ov_outputs = ov_model(**tokens)
+        self.assertTrue("start_logits" in ov_outputs)
+        self.assertTrue("end_logits" in ov_outputs)
+        self.assertIsInstance(ov_outputs.start_logits, torch.Tensor)
+        self.assertIsInstance(ov_outputs.end_logits, torch.Tensor)
+        with torch.no_grad():
+            transformers_outputs = transformers_model(**tokens)
+        self.assertTrue(torch.allclose(ov_outputs.start_logits, transformers_outputs.start_logits, atol=1e-4))
+        self.assertTrue(torch.allclose(ov_outputs.end_logits, transformers_outputs.end_logits, atol=1e-4))
+        gc.collect()
+
+    @parameterized.expand(SUPPORTED_ARCHITECTURES)
+    def test_pipeline(self, model_arch):
+        model_id = MODEL_NAMES[model_arch]
+        model = OVModelForQuestionAnswering.from_pretrained(model_id, from_transformers=True)
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        pipe = pipeline("question-answering", model=model, tokenizer=tokenizer)
+        question = "What's my name?"
+        context = "My Name is Arthur and I live in Lyon."
+        outputs = pipe(question, context)
+        self.assertEqual(pipe.device, model.device)
+        self.assertGreaterEqual(outputs["score"], 0.0)
+        self.assertIsInstance(outputs["answer"], str)
+        gc.collect()
+
+
+class OVModelForTokenClassificationIntegrationTest(unittest.TestCase):
+    SUPPORTED_ARCHITECTURES = (
+        "distilbert",
+        "bert",
+        "roberta",
+    )
+
+    @parameterized.expand(SUPPORTED_ARCHITECTURES)
+    def test_compare_to_transformers(self, model_arch):
+        model_id = MODEL_NAMES[model_arch]
+        set_seed(SEED)
+        ov_model = OVModelForTokenClassification.from_pretrained(model_id, from_transformers=True)
+        self.assertIsInstance(ov_model.config, PretrainedConfig)
+        transformers_model = AutoModelForTokenClassification.from_pretrained(model_id)
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        tokens = tokenizer("This is a sample input", return_tensors="pt")
+        ov_outputs = ov_model(**tokens)
+        self.assertTrue("logits" in ov_outputs)
+        self.assertIsInstance(ov_outputs.logits, torch.Tensor)
+        with torch.no_grad():
+            transformers_outputs = transformers_model(**tokens)
+        self.assertTrue(torch.allclose(ov_outputs.logits, transformers_outputs.logits, atol=1e-4))
+        gc.collect()
+
+    @parameterized.expand(SUPPORTED_ARCHITECTURES)
+    def test_pipeline(self, model_arch):
+        model_id = MODEL_NAMES[model_arch]
+        model = OVModelForTokenClassification.from_pretrained(model_id, from_transformers=True)
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        pipe = pipeline("token-classification", model=model, tokenizer=tokenizer)
+        outputs = pipe("My Name is Arthur and I live in Lyon.")
+        self.assertEqual(pipe.device, model.device)
+        self.assertTrue(all(item["score"] > 0.0 for item in outputs))
+        gc.collect()
