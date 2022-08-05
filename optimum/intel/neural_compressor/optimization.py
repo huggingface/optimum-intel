3bc1e6439bb8c31ad08dcf121a32db2489a9bc22
Ella Charlaix <ella@huggingface.co>
2022-08-05 11:50:05 +0200

    Fix style

--- optimum/intel/neural_compressor/optimization.py
+++ optimum/intel/neural_compressor/optimization.py
@@ -17,7 +17,7 @@ import os
 from typing import Callable, Optional, Union
 
 import torch
-from transformers import PreTrainedModel, AutoModel
+from transformers import AutoModel, PreTrainedModel
 
 from neural_compressor.experimental import Pruning, Quantization, common
 from neural_compressor.experimental.scheduler import Scheduler
