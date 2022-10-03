f829b225120eec218601968b8682a1faedaa5fe8
Ella Charlaix <ella@huggingface.co>
2022-10-03 08:37:55 +0000

    Replace accuracy test with inference test

--- tests/openvino/test_quantization.py
+++ tests/openvino/test_quantization.py
@@ -16,18 +16,18 @@ import tempfile
 import unittest
 from functools import partial
 
-from datasets import load_dataset
-from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
-from evaluate import evaluator
-from optimum.intel.openvino.modeling import OVModelForQuestionAnswering
+from optimum.intel.openvino.modeling import OVModelForSequenceClassification
 from optimum.intel.openvino.nncf_config import DEFAULT_QUANTIZATION_CONFIG
 from optimum.intel.openvino.quantization import OVQuantizer
 from parameterized import parameterized
 
 
 class OVQuantizerTest(unittest.TestCase):
-    SUPPORTED_ARCHITECTURES_WITH_EXPECTED_QUANTIZED_MATMULS = (("distilbert-base-cased-distilled-squad", 80),)
+    SUPPORTED_ARCHITECTURES_WITH_EXPECTED_QUANTIZED_MATMULS = (
+        ("distilbert-base-uncased-finetuned-sst-2-english", 82),
+    )
 
     @parameterized.expand(SUPPORTED_ARCHITECTURES_WITH_EXPECTED_QUANTIZED_MATMULS)
     def test_static_quantization(self, model_name, expected_fake_quantize):
@@ -35,14 +35,14 @@ class OVQuantizerTest(unittest.TestCase):
             return tokenizer(examples["sentence"], padding="max_length", max_length=128, truncation=True)
 
         with tempfile.TemporaryDirectory() as tmp_dir:
-            transformers_model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+            transformers_model = AutoModelForSequenceClassification.from_pretrained(model_name)
             tokenizer = AutoTokenizer.from_pretrained(model_name)
             quantizer = OVQuantizer.from_pretrained(transformers_model)
             calibration_dataset = quantizer.get_calibration_dataset(
                 "glue",
                 dataset_config_name="sst2",
                 preprocess_function=partial(preprocess_function, tokenizer=tokenizer),
-                num_samples=40,
+                num_samples=10,
                 dataset_split="train",
             )
             quantizer.quantize(
@@ -50,7 +50,7 @@ class OVQuantizerTest(unittest.TestCase):
                 quantization_config=DEFAULT_QUANTIZATION_CONFIG,
                 calibration_dataset=calibration_dataset,
             )
-            ov_model = OVModelForQuestionAnswering.from_pretrained(tmp_dir)
+            ov_model = OVModelForSequenceClassification.from_pretrained(tmp_dir)
 
             num_fake_quantize = 0
             for elem in ov_model.model.get_ops():
@@ -58,11 +58,6 @@ class OVQuantizerTest(unittest.TestCase):
                     num_fake_quantize += 1
             self.assertEqual(expected_fake_quantize, num_fake_quantize)
 
-            data = load_dataset("squad", split="validation").select(range(50))
-            eval = evaluator("question-answering")
-            transformers_pipe = pipeline("question-answering", model=transformers_model, tokenizer=tokenizer)
-            ov_pipe = pipeline("question-answering", model=ov_model, tokenizer=tokenizer)
-            transformers_metric = eval.compute(model_or_pipeline=transformers_pipe, data=data, metric="squad")
-            ov_metric = eval.compute(model_or_pipeline=ov_pipe, data=data, metric="squad")
-
-            self.assertGreaterEqual(ov_metric["f1"], transformers_metric["f1"] * 0.70)
+            tokens = tokenizer("This is a sample input", return_tensors="pt")
+            outputs = ov_model(**tokens)
+            self.assertTrue("logits" in outputs)
