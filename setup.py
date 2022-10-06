0f4ae16464759f5a18a869a537f3820a66ec2f56
Ella Charlaix <ella@huggingface.co>
2022-10-06 07:46:09 +0000

    Add nncf install extra

--- setup.py
+++ setup.py
@@ -30,7 +30,8 @@ QUALITY_REQUIRES = [
 
 EXTRAS_REQUIRE = {
     "neural-compressor": "neural-compressor",
-    "openvino": ["openvino>=2022.2.0", "nncf"],
+    "openvino": ["openvino>=2022.2.0"],
+    "nncf": ["nncf"],
     "quality": QUALITY_REQUIRES,
     "tests": TESTS_REQUIRE,
 }
