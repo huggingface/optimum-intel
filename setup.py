b93c012c9e1181d63c472dd678b043f013160c4d
Ella Charlaix <80481427+echarlaix@users.noreply.github.com>
2022-11-29 10:14:30 +0100

    Fix style (#121)

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ QUALITY_REQUIRES = [
 
 EXTRAS_REQUIRE = {
     "neural-compressor": "neural-compressor>=1.13.0",
-    "openvino": ["openvino>=2022.2.0","transformers>=4.20.0,<4.24.1"],
+    "openvino": ["openvino>=2022.2.0", "transformers>=4.20.0,<4.24.1"],
     "nncf": ["nncf"],
     "quality": QUALITY_REQUIRES,
     "tests": TESTS_REQUIRE,
