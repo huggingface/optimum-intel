650203a84b06fd18aeb998c3055f194b6efe6d06
Morgan Funtowicz <morgan@huggingface.co>
2022-12-01 14:58:37 +0100

    Implement default fallback in case of Exception for optimized model.

--- optimum/intel/ipex/inference.py
+++ optimum/intel/ipex/inference.py
@@ -5,12 +5,41 @@ from transformers.pipelines import Pipeline
 from typing import Union
 
 
-class inference_mode:
-    __slots__ = ("_model", "_verbose", "_use_onednn", "_original")
+class _ModelFallbackWrapper:
 
-    def __init__(self, model: Union[nn.Module, Pipeline], verbose: bool = False, use_onednn: bool = True):
+    __slots__ = ("_optimized", "_default")
+
+    def __init__(self, optimized, default):
+        self._optimized = optimized
+        self._default = default
+
+    def __call__(self, *args, **kwargs):
+        try:
+            return self._optimized(*args, **kwargs)
+        except Exception:
+            return self._default(*args, **kwargs)
+
+    def __getattr__(self, item):
+        if not item.startswith("__"):
+            return getattr(self._default, item)
+        else:
+            return self.item
+
+
+class inference_mode:
+    __slots__ = ("_model", "_dtype", "_graph_mode", "_use_onednn", "_verbose", "_original")
+
+    def __init__(
+        self,
+        model: Union[nn.Module, Pipeline],
+        dtype: torch.dtype = torch.float32,
+        use_onednn: bool = True,
+        verbose: bool = False
+    ):
         self._model = model
         self._verbose = ipex.utils.verbose.VERBOSE_ON if verbose else ipex.utils.verbose.VERBOSE_OFF
+        self._dtype = dtype
+        self._graph_mode = False  # Let's keep for future use when it doesn't hang anymore
         self._use_onednn = use_onednn
         self._original = None
 
@@ -21,22 +50,28 @@ class inference_mode:
                 ipex.enable_onednn_fusion(True)
                 if isinstance(self._model, Pipeline):
                     self._original = self._model.model
+
                     model = ipex.optimize(
-                        self._model.model.to(memory_format=torch.channels_last),
-                        dtype=torch.float32,
+                        self._model.model,
+                        dtype=self._dtype,
+                        graph_mode=self._graph_mode,
                         level="O1",
                         auto_kernel_selection=True
                     )
-                    self._model.model = model  # Patching model with the new one
+
+                    # Patching model with the new one
+                    self._model.model = _ModelFallbackWrapper(model, self._original)
                     return self._model
                 else:
                     self._original = self._model
                     model = ipex.optimize(
-                        self._model.to(memory_format=torch.channels_last),
-                        dtype=torch.float32,
+                        self._model,
+                        dtype=self._dtype,
+                        graph_mode=self._graph_mode,
                         level="O1",
                         auto_kernel_selection=True
                     )
+
                     return model
 
     def __exit__(self, exc_type, exc_val, exc_tb):
