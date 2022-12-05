b74a77873cc61f3ad35742197fd7f5702d50cd21
Morgan Funtowicz <morgan@huggingface.co>
2022-12-05 09:13:13 +0100

    Move IPEx to optional dependency

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ INSTALL_REQUIRES = [
     "torch",
     "sentencepiece",
     "scipy",
-    "intel_extension_for_pytorch"
 ]
 
 TESTS_REQUIRE = ["pytest", "parameterized", "Pillow", "evaluate"]
@@ -33,6 +32,7 @@ EXTRAS_REQUIRE = {
     "neural-compressor": "neural-compressor>=1.13.0",
     "openvino": ["openvino>=2022.2.0"],
     "nncf": ["nncf"],
+    "ipex": ["intel_extension_for_pytorch"],
     "quality": QUALITY_REQUIRES,
     "tests": TESTS_REQUIRE,
 }
