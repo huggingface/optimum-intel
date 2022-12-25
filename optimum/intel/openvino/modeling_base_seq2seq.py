2936d248e0772899be9741cb0287496ae5926de7
Jan Iwaszkiewicz <jan.iwaszkiewicz@intel.com>
2022-12-26 00:04:49 +0100

    OpenVINO Seq2Seq pipeline improvements (#131)
    * Add async mode and shared memory
    
    * Fixes and codestyle
    
    * Add shared_memory helper

--- optimum/intel/openvino/modeling_base_seq2seq.py
+++ optimum/intel/openvino/modeling_base_seq2seq.py
@@ -66,7 +66,7 @@ class OVBaseModelForSeq2SeqLM(OVBaseModel):
         self.model_save_dir = kwargs.get("model_save_dir")
         self._device = kwargs.get("device", "CPU")
         self.is_dynamic = kwargs.get("dynamic_shapes", True)
-        self.ov_config = {"PERFORMANCE_HINT": "LATENCY"}
+        self.ov_config = {}
         if "GPU" in self._device:
             raise ValueError("Support of dynamic shapes for GPU devices is not yet available.")
         if self.is_dynamic:
