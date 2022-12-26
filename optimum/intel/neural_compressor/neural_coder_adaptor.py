6f52a76e0c6dbe2a62f652e65783b2b84ad42168
kaiyaointel <kai.yao@intel.com>
2022-12-26 14:24:58 +0800

    enable neural coder adaptor for optimum-intel

---
+++ optimum/intel/neural_compressor/neural_coder_adaptor.py
@@ -0,0 +1,31 @@
+class APIAdapter():
+	"""API design adaption for Neural Coder"""
+    
+    default_quant_dynamic = \
+f"""
+def eval_func(model):
+    EVAL_FUNC_LINES
+from optimum.intel.neural_compressor import IncOptimizer
+from optimum.intel.neural_compressor import IncQuantizationConfig
+from optimum.intel.neural_compressor import IncQuantizer
+quantization_config = OPTIMUM_QUANT_CONFIG
+quantizer = IncQuantizer(quantization_config, eval_func=eval_func)
+optimizer = IncOptimizer(MODEL_NAME, quantizer=quantizer)
+MODEL_NAME = optimizer.fit()
+MODEL_NAME.eval()
+"""
+
+    default_quant_static = \
+f"""
+def eval_func(model):
+    EVAL_FUNC_LINES
+from optimum.intel.neural_compressor import IncOptimizer
+from optimum.intel.neural_compressor import IncQuantizationConfig
+from optimum.intel.neural_compressor import IncQuantizer
+quantization_config = OPTIMUM_QUANT_CONFIG
+calib_dataloader = DATALOADER_NAME
+quantizer = IncQuantizer(quantization_config, eval_func=eval_func, calib_dataloader=calib_dataloader)
+optimizer = IncOptimizer(MODEL_NAME, quantizer=quantizer)
+MODEL_NAME = optimizer.fit()
+MODEL_NAME.eval()
+"""
