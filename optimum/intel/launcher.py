d85b8b2cee954c00f4cd43fc35144073f5219333
kaiyaointel <kai.yao@intel.com>
2022-12-27 10:20:58 +0800

    debug

--- optimum/intel/launcher.py
+++ optimum/intel/launcher.py
@@ -3,7 +3,10 @@ from .neural_compressor.neural_coder_adaptor import NeuralCoderAdaptor
 
 args = Launcher.parse_args()
 
+modular_pattern = {}
+
 modular_pattern["pytorch_inc_static_quant_fx"] = NeuralCoderAdaptor.default_quant_static
 modular_pattern["pytorch_inc_dynamic_quant"] = NeuralCoderAdaptor.default_quant_dynamic
+modular_pattern["inc_auto"] = NeuralCoderAdaptor.default_quant_dynamic
 
 Launcher.execute(args, use_modular=True, modular_pattern=modular_pattern)
