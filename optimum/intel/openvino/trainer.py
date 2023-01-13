7724ce762663af7b37011572f1a8bd1ad0fc3b49
Vui Seng Chua <vui.seng.chua@intel.com>
2023-01-13 14:55:49 -0800

    Revert IR generation with static input shape for joint compression

--- optimum/intel/openvino/trainer.py
+++ optimum/intel/openvino/trainer.py
@@ -740,12 +740,6 @@ class OVTrainer(Trainer):
                 "Error encountered during IR generation. Run continues, please check compression config and model.onnx"
             )
         else:
-            # Configure IR model to accept dynamic-shaped input(s)
-            dynamic_iport_cfg = dict()
-            for iport in ov_model.inputs:
-                dynamic_iport_cfg[iport.any_name] = PartialShape([-1] * len(iport.shape))
-            ov_model.reshape(dynamic_iport_cfg)
-
             xml_pth = os.path.join(os.path.dirname(onnx_model), OV_XML_FILE_NAME)
             bin_pth = xml_pth.replace(".xml", ".bin")
             openvino.runtime.serialize(ov_model, xml_pth, bin_pth)
