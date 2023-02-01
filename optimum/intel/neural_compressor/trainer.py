56be6186b5ca6defcf57ed41d2bb887e850e543a
Ella Charlaix <ella@huggingface.co>
2023-02-01 15:39:59 +0100

    Fix evaluation step for quantized models

--- optimum/intel/neural_compressor/trainer.py
+++ optimum/intel/neural_compressor/trainer.py
@@ -18,7 +18,8 @@ import os
 import sys
 import time
 import warnings
-from typing import TYPE_CHECKING, Dict, List, Optional, Union
+from collections.abc import Mapping
+from typing import TYPE_CHECKING, Any, Callable, Dict, List, Optional, Tuple, Union
 
 import datasets
 import torch
@@ -651,6 +652,7 @@ class INCTrainer(Trainer):
         else:
             labels = None
         teacher_outputs = inputs.pop("teacher_logits", None)
+        inputs = self._prepare_input(inputs, model.device)
         outputs = model(**inputs)
 
         # Save past state if it exists
@@ -693,6 +695,24 @@ class INCTrainer(Trainer):
                     outputs[0] = loss
 
         return (loss, outputs) if return_outputs else loss
+
+    def _prepare_input(self, data: Union[torch.Tensor, Any], device: torch.device = None) -> Union[torch.Tensor, Any]:
+        """
+        Prepares one `data` before feeding it to the model, be it a tensor or a nested list/dictionary of tensors.
+        """
+        if isinstance(data, Mapping):
+            return type(data)({k: self._prepare_input(v, device) for k, v in data.items()})
+        elif isinstance(data, (tuple, list)):
+            return type(data)(self._prepare_input(v, device) for v in data)
+        elif isinstance(data, torch.Tensor):
+            kwargs = dict(device=device or self.args.device)
+            if self.deepspeed and data.dtype != torch.int64:
+                # NLP models inputs are int64 and those get adjusted to the right dtype of the
+                # embedding. Other models such as wav2vec2's inputs are already float and thus
+                # may need special handling to match the dtypes of the model
+                kwargs.update(dict(dtype=self.args.hf_deepspeed_config.dtype()))
+            return data.to(**kwargs)
+        return data
 
     @staticmethod
     def _get_logits(model_outputs):
