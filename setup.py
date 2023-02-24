161f24e07e3ee4371d5f7bb06f0809c6a181efb3
Alexander Kozlov <alexander.kozlov@intel.com>
2023-02-24 13:40:42 +0400

    Update setup.py
    Co-authored-by: Helena Kloosterman <helena.kloosterman@intel.com>

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ QUALITY_REQUIRE = ["black==22.3", "isort>=5.5.4"]
 
 EXTRAS_REQUIRE = {
     "neural-compressor": ["neural-compressor>=2.0.0", "onnx", "onnxruntime"],
-    "openvino": ["openvino==2023.0.0.dev20230217", "onnx", "onnxruntime"],
+    "openvino": ["openvino>=2023.0.0.dev20230217", "onnx", "onnxruntime"],
     "nncf": [
         "nncf>=2.4.0",
         "openvino-dev==2023.0.0.dev20230217",
