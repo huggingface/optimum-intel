9cadd1eaa3defdec2310dd940cb8155750e3b75c
kaiyaointel <kai.yao@intel.com>
2023-03-01 11:06:41 +0800

    add UT for NeuralCoder launcher

--- tests/neural_compressor/test_neural_coder_adaptor.py
+++ tests/neural_compressor/test_neural_coder_adaptor.py
@@ -1,6 +1,8 @@
 import unittest
 
+from argparse import Namespace
 from optimum.intel.neural_compressor.neural_coder_adaptor import NeuralCoderAdaptor
+from neural_coder.launcher import Launcher
 
 
 class NeuralCoderAdaptorTest(unittest.TestCase):
@@ -9,3 +11,25 @@ class NeuralCoderAdaptorTest(unittest.TestCase):
         static_api = NeuralCoderAdaptor.default_quant_static
         self.assertEqual(type(dynamic_api), type(""))
         self.assertEqual(type(static_api), type(""))
+
+    def test_launcher(self):
+        args = Namespace(
+            opt='',
+            approach='auto',
+            config='',
+            bench=False,
+            enable=True,
+            script='run_glue_source.py',
+        )
+
+        modular_pattern = {}
+
+        modular_pattern["pytorch_inc_static_quant_fx"] = NeuralCoderAdaptor.default_quant_static
+        modular_pattern["pytorch_inc_dynamic_quant"] = NeuralCoderAdaptor.default_quant_dynamic
+        modular_pattern["inc_auto"] = NeuralCoderAdaptor.default_quant_dynamic
+
+        Launcher.execute(args, use_modular=True, modular_pattern=modular_pattern, use_inc=False)
+        
+        # determine if the code optimization is correct
+        import filecmp
+        self.assertEqual(True, filecmp.cmp('run_glue_target.py', 'run_glue_source_optimized.py'))
