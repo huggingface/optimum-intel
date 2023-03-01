adfc13ff40d901cf6cd673cd02faa780eaf4dabc
kaiyaointel <kai.yao@intel.com>
2023-03-01 11:32:31 +0800

    fix code format

--- tests/neural_compressor/test_neural_coder_adaptor.py
+++ tests/neural_compressor/test_neural_coder_adaptor.py
@@ -6,37 +6,31 @@ from neural_coder.launcher import Launcher
 
 
 class NeuralCoderAdaptorTest(unittest.TestCase):
-
     def test_string_type(self):
         dynamic_api = NeuralCoderAdaptor.default_quant_dynamic
         static_api = NeuralCoderAdaptor.default_quant_static
-        self.assertEqual(type(dynamic_api), type(''))
-        self.assertEqual(type(static_api), type(''))
+        self.assertEqual(type(dynamic_api), type(""))
+        self.assertEqual(type(static_api), type(""))
 
     def test_launcher(self):
         args = Namespace(
-            opt='',
-            approach='auto',
-            config='',
+            opt="",
+            approach="auto",
+            config="",
             bench=False,
             enable=True,
-            script='run_glue_source.py',
-            )
+            script="run_glue_source.py",
+        )
 
         modular_pattern = {}
 
-        modular_pattern['pytorch_inc_static_quant_fx'] = \
-            NeuralCoderAdaptor.default_quant_static
-        modular_pattern['pytorch_inc_dynamic_quant'] = \
-            NeuralCoderAdaptor.default_quant_dynamic
-        modular_pattern['inc_auto'] = \
-            NeuralCoderAdaptor.default_quant_dynamic
+        modular_pattern["pytorch_inc_static_quant_fx"] = NeuralCoderAdaptor.default_quant_static
+        modular_pattern["pytorch_inc_dynamic_quant"] = NeuralCoderAdaptor.default_quant_dynamic
+        modular_pattern["inc_auto"] = NeuralCoderAdaptor.default_quant_dynamic
 
-        Launcher.execute(args, use_modular=True,
-                         modular_pattern=modular_pattern, use_inc=False)
+        Launcher.execute(args, use_modular=True, modular_pattern=modular_pattern, use_inc=False)
 
         # determine if the code optimization is correct
-
         import filecmp
-        self.assertEqual(True, filecmp.cmp('run_glue_target.py',
-                         'run_glue_source_optimized.py'))
+
+        self.assertEqual(True, filecmp.cmp("run_glue_target.py", "run_glue_source_optimized.py"))
