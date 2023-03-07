4c7f2447d77b6337a3d9c869f3d02a544f00b61a
Pan, Yujie <yujie.pan@intel.com>
2023-03-06 19:52:34 -0800

    rtol=0.001

--- tests/openvino/test_training.py
+++ tests/openvino/test_training.py
@@ -368,7 +368,7 @@ class OVTrainerTrainingTest(unittest.TestCase):
                     torch.allclose(
                         torch.softmax(ovmodel_logits, dim=-1),
                         torch.softmax(torch_logits, dim=-1),
-                        rtol=0.01,
+                        rtol=0.001,
                     )
                 )
 
