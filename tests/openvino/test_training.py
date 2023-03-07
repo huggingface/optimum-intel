c92043ca79423e1309dc8588a994cf025e81fa86
Pan, Yujie <yujie.pan@intel.com>
2023-03-06 22:01:44 -0800

    set rtol to 0.0001

--- tests/openvino/test_training.py
+++ tests/openvino/test_training.py
@@ -398,7 +398,7 @@ class OVTrainerTrainingTest(unittest.TestCase):
                     torch.allclose(
                         torch.softmax(ovmodel_logits, dim=-1),
                         torch.softmax(torch_logits, dim=-1),
-                        rtol=0.001,
+                        rtol=0.0001,
                     )
                 )
 
