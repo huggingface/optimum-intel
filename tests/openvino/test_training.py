c23f2c0c809495943173bc22afcc593a04d54dfa
Pan, Yujie <yujie.pan@intel.com>
2023-03-13 16:56:05 +0800

    add test for swin

--- tests/openvino/test_training.py
+++ tests/openvino/test_training.py
@@ -23,8 +23,14 @@ from typing import Dict, List, Optional, Union
 
 import numpy as np
 import torch
+import torchvision
 from datasets import load_dataset
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from transformers import (
+    AutoFeatureExtractor,
+    AutoModelForImageClassification,
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+)
 from transformers.trainer_utils import TrainOutput
 from transformers.utils import WEIGHTS_NAME
 
@@ -34,7 +40,7 @@ from nncf.experimental.torch.sparsity.movement.algo import MovementSparsityContr
 from openvino.runtime import PartialShape
 from optimum.intel.openvino import OVTrainingArguments
 from optimum.intel.openvino.configuration import DEFAULT_QUANTIZATION_CONFIG, OVConfig
-from optimum.intel.openvino.modeling import OVModelForSequenceClassification
+from optimum.intel.openvino.modeling import OVModelForImageClassification, OVModelForSequenceClassification
 from optimum.intel.openvino.trainer import OVTrainer
 from optimum.intel.openvino.utils import OV_XML_FILE_NAME
 from parameterized import parameterized
@@ -248,7 +254,7 @@ OVTRAINER_TRAINING_TEST_DESCRIPTORS = {
 }
 
 
-class OVTrainerTrainingTest(unittest.TestCase):
+class OVTrainerTextClassificationTrainingTest(unittest.TestCase):
     @parameterized.expand(OVTRAINER_TRAINING_TEST_DESCRIPTORS.items())
     def test_training(self, _, desc: OVTrainerTestDescriptor):
         self.prepare(desc)
@@ -429,4 +435,227 @@ class OVTrainerTrainingTest(unittest.TestCase):
         _assertInputsEqual(irmodel, shape=[bs, sl])
 
         irmodel = _reshape_ir_by_input_shape(irmodel, batch_size=-1, seqlen=-1)
+        self.assertTrue(irmodel.is_dynamic())
+
+
+STRUCTURED_MOVEMENT_SPARSITY_CONFIG_FOR_SWIN = {
+    "algorithm": "movement_sparsity",
+    "params": {
+        "warmup_start_epoch": 1,
+        "warmup_end_epoch": 2,
+        "importance_regularization_factor": 1.0,
+        "enable_structured_masking": True,
+    },
+    "sparse_structure_by_scopes": [
+        {"mode": "block", "sparse_factors": [8, 8], "target_scopes": "{re}.*SwinAttention.*"},
+        {"mode": "per_dim", "axis": 0, "target_scopes": "{re}.*SwinIntermediate.*"},
+        {"mode": "per_dim", "axis": 1, "target_scopes": "{re}.*SwinOutput.*"},
+    ],
+    "ignored_scopes": ["{re}.*PatchEmbed.*", "{re}.*PatchMerging.*", "{re}.*classifier.*", "{re}.*LayerNorm.*"],
+}
+UNSTRUCTURED_MOVEMENT_SPARSITY_CONFIG_FOR_SWIN = deepcopy(STRUCTURED_MOVEMENT_SPARSITY_CONFIG_FOR_SWIN)
+UNSTRUCTURED_MOVEMENT_SPARSITY_CONFIG_FOR_SWIN["params"]["enable_structured_masking"] = False
+
+OVTRAINER_IMAGE_CLASSIFICATION_TEST_DESCRIPTORS = {
+    "default_quantization": OVTrainerTestDescriptor(
+        model_id="yujiepan/tiny-random-SwinModel",
+        nncf_compression_config=[DEFAULT_QUANTIZATION_CONFIG],
+        expected_fake_quantize=27,
+        expected_int8=27,
+        compression_metrics=["compression_loss"],
+    ),
+    "structured_movement_sparsity": OVTrainerTestDescriptor(
+        model_id="yujiepan/tiny-random-SwinModel",
+        nncf_compression_config=[STRUCTURED_MOVEMENT_SPARSITY_CONFIG_FOR_SWIN],
+        expected_binary_masks=48,
+        compression_metrics=["compression_loss"],
+    ),
+    "unstructured_movement_sparsity": OVTrainerTestDescriptor(
+        model_id="yujiepan/tiny-random-SwinModel",
+        nncf_compression_config=[UNSTRUCTURED_MOVEMENT_SPARSITY_CONFIG_FOR_SWIN],
+        expected_binary_masks=48,
+        compression_metrics=["compression_loss"],
+    ),
+    "default_quantization,structured_movement_sparsity": OVTrainerTestDescriptor(
+        model_id="yujiepan/tiny-random-SwinModel",
+        nncf_compression_config=[DEFAULT_QUANTIZATION_CONFIG, STRUCTURED_MOVEMENT_SPARSITY_CONFIG_FOR_SWIN],
+        expected_fake_quantize=27,
+        expected_int8=27,
+        expected_binary_masks=48,
+        compression_metrics=["compression_loss"],
+    ),
+    "default_quantization,unstructured_movement_sparsity": OVTrainerTestDescriptor(
+        model_id="yujiepan/tiny-random-SwinModel",
+        nncf_compression_config=[DEFAULT_QUANTIZATION_CONFIG, UNSTRUCTURED_MOVEMENT_SPARSITY_CONFIG_FOR_SWIN],
+        expected_fake_quantize=27,
+        expected_int8=27,
+        expected_binary_masks=48,
+        compression_metrics=["compression_loss"],
+    ),
+    "distillation,default_quantization,structured_movement_sparsity": OVTrainerTestDescriptor(
+        model_id="yujiepan/tiny-random-SwinModel",
+        teacher_model_id="yujiepan/tiny-random-SwinModel",
+        nncf_compression_config=[DEFAULT_QUANTIZATION_CONFIG, STRUCTURED_MOVEMENT_SPARSITY_CONFIG_FOR_SWIN],
+        expected_fake_quantize=27,
+        expected_int8=27,
+        expected_binary_masks=48,
+        compression_metrics=["compression_loss", "distillation_loss", "task_loss"],
+    ),
+    "distillation,default_quantization,unstructured_movement_sparsity": OVTrainerTestDescriptor(
+        model_id="yujiepan/tiny-random-SwinModel",
+        teacher_model_id="yujiepan/tiny-random-SwinModel",
+        nncf_compression_config=[DEFAULT_QUANTIZATION_CONFIG, UNSTRUCTURED_MOVEMENT_SPARSITY_CONFIG_FOR_SWIN],
+        expected_fake_quantize=27,
+        expected_int8=27,
+        expected_binary_masks=48,
+        compression_metrics=["compression_loss", "distillation_loss", "task_loss"],
+    ),
+}
+
+
+class OVTrainerImageClassificationTrainingTest(unittest.TestCase):
+    @parameterized.expand(OVTRAINER_IMAGE_CLASSIFICATION_TEST_DESCRIPTORS.items())
+    def test_training(self, _, desc: OVTrainerTestDescriptor):
+        self.prepare(desc)
+        num_train_epochs = 3
+        train_batch_size = 4
+        total_steps = ceil(len(self.train_dataset) / train_batch_size) * num_train_epochs
+        with tempfile.TemporaryDirectory() as output_dir:
+            self.args = OVTrainingArguments(
+                output_dir=output_dir,
+                num_train_epochs=num_train_epochs,
+                learning_rate=1e-7,
+                do_train=True,
+                do_eval=True,
+                logging_steps=1,
+                per_device_train_batch_size=train_batch_size,
+                per_device_eval_batch_size=1,
+                no_cuda=True,
+                full_determinism=True,
+                remove_unused_columns=False,
+            )
+            self.trainer = OVTrainer(
+                model=self.model,
+                teacher_model=self.teacher_model,
+                args=self.args,
+                ov_config=self.ov_config,
+                task=self.task,
+                train_dataset=self.train_dataset,
+                eval_dataset=self.eval_dataset,
+                tokenizer=self.feature_extractor,
+                data_collator=self.collate_fn,
+                compute_metrics=self.compute_metric,
+            )
+
+            trainer = self.trainer
+            movement_controller = trainer._get_compression_controller_by_cls(
+                MovementSparsityController
+            )  # pylint: disable=protected-access
+            if movement_controller is not None:
+                # make sure the binary masks will have many zeros
+                initialize_movement_sparsifier_parameters_by_sparsity(movement_controller, sparsity=0.95)
+
+            # check evaluation can work even before training.
+            metrics = trainer.evaluate()
+            self.assertIn("eval_loss", metrics)
+            self.assertIn("eval_accuracy", metrics)
+
+            # check trainining
+            train_outputs = trainer.train()
+            self.assertIsInstance(train_outputs, TrainOutput)
+            self.assertEqual(train_outputs.global_step, total_steps)
+            self.assertEqual(sorted(desc.compression_metrics), sorted(trainer.compression_metrics.keys()))
+
+            # check model can be saved
+            trainer.save_model()
+            self.assertTrue(Path(output_dir, WEIGHTS_NAME).is_file())
+            self.assertTrue(Path(output_dir, OV_XML_FILE_NAME).is_file())
+
+            # check saved ovmodel IR and output
+            ovmodel = OVModelForImageClassification.from_pretrained(output_dir)
+            self.check_irmodel_is_dynamic(ovmodel.model)
+            self.check_ovmodel_output_equals_torch_output(ovmodel, trainer.model)
+
+            # check ovmodel quantization ops
+            num_fake_quantize, num_int8 = self.count_quantization_op_number(ovmodel)
+            self.assertEqual(desc.expected_fake_quantize, num_fake_quantize)
+            self.assertEqual(desc.expected_int8, num_int8)
+
+            # check binary mask in sparsity/pruning algorithms
+            state_dict = torch.load(Path(output_dir, WEIGHTS_NAME), map_location="cpu")
+            num_binary_masks = sum(key.endswith("_binary_mask") for key in state_dict)
+            self.assertEqual(desc.expected_binary_masks, num_binary_masks)
+
+    def prepare(self, desc: OVTrainerTestDescriptor):
+        torch.manual_seed(42)
+        self.ov_config = OVConfig()
+        self.ov_config.compression = desc.nncf_compression_config
+        self.task = "image-classification"
+        self.feature_extractor = AutoFeatureExtractor.from_pretrained(desc.model_id)
+        self.model = AutoModelForImageClassification.from_pretrained(desc.model_id, num_labels=3)
+        self.teacher_model = None
+        if desc.teacher_model_id:
+            self.teacher_model = AutoModelForImageClassification.from_pretrained(desc.teacher_model_id, num_labels=3)
+
+        if "shortest_edge" in self.feature_extractor.size:
+            size = self.feature_extractor.size["shortest_edge"]
+        else:
+            size = (self.feature_extractor.size["height"], self.feature_extractor.size["width"])
+        image_transforms = torchvision.transforms.Compose(
+            [
+                torchvision.transforms.RandomResizedCrop(size),
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize(
+                    mean=self.feature_extractor.image_mean, std=self.feature_extractor.image_std
+                ),
+            ]
+        )
+
+        def data_transform(batch):
+            batch["pixel_values"] = [image_transforms(pil_img.convert("RGB")) for pil_img in batch["image"]]
+            return batch
+
+        def collate_fn(examples):
+            pixel_values = torch.stack([example["pixel_values"] for example in examples])
+            labels = torch.tensor([example["labels"] for example in examples])
+            return {"pixel_values": pixel_values, "labels": labels}
+
+        self.dataset = load_dataset("beans", task="image-classification")
+        self.dataset.set_transform(data_transform)
+        self.train_dataset = self.dataset["train"].select(range(8))
+        self.eval_dataset = self.dataset["validation"].select(range(8))
+        self.data_transform = data_transform
+        self.collate_fn = collate_fn
+        self.metric = evaluate.load("accuracy")
+        self.compute_metric = lambda p: self.metric.compute(
+            predictions=np.argmax(p.predictions, axis=1), references=p.label_ids
+        )
+
+    def count_quantization_op_number(self, ovmodel):
+        num_fake_quantize = 0
+        num_int8 = 0
+        for elem in ovmodel.model.get_ops():
+            if "FakeQuantize" in elem.name:
+                num_fake_quantize += 1
+            for i in range(elem.get_output_size()):
+                if "8" in elem.get_output_element_type(i).get_type_name():
+                    num_int8 += 1
+        return num_fake_quantize, num_int8
+
+    def check_ovmodel_output_equals_torch_output(self, ovmodel, torch_model):
+        torch_model = torch_model.eval()
+        for inputs in self.trainer.get_eval_dataloader():
+            ovmodel_outputs = ovmodel(**inputs)
+            self.assertIn("logits", ovmodel_outputs)
+            ovmodel_logits = ovmodel_outputs.logits
+            torch_logits = torch_model(**inputs).logits
+            self.assertTrue(
+                torch.allclose(
+                    torch.softmax(ovmodel_logits, dim=-1),
+                    torch.softmax(torch_logits, dim=-1),
+                    rtol=1e-4,
+                )
+            )
+
+    def check_irmodel_is_dynamic(self, irmodel):
         self.assertTrue(irmodel.is_dynamic())
