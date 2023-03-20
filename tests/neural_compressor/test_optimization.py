d593335bdb444b4717a0adb62a0383d1fa424625
Ella Charlaix <ella@huggingface.co>
2023-03-20 16:07:36 +0100

    fix imports

--- tests/neural_compressor/test_optimization.py
+++ tests/neural_compressor/test_optimization.py
@@ -42,8 +42,7 @@ from neural_compressor.config import (
     WeightPruningConfig,
 )
 from onnx import load as onnx_load
-from optimum.intel import INCQuantizer
-from optimum.intel.neural_compressor import INCModelForQuestionAnswering, INCModelForSequenceClassification, INCTrainer
+from optimum.intel import INCModelForQuestionAnswering, INCModelForSequenceClassification, INCQuantizer, INCTrainer
 from optimum.onnxruntime import ORTModelForQuestionAnswering, ORTModelForSequenceClassification
 
 
