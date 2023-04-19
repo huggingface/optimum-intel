dd9ea04ef6fa4670005a97911c7acb39339d2873
Ella Charlaix <ella@huggingface.co>
2023-04-19 18:37:53 +0200

    fix dates

--- examples/neural_compressor/text-generation/run_generation.py
+++ examples/neural_compressor/text-generation/run_generation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
 # Copyright 2018 Google AI, Google Brain and Carnegie Mellon University Authors and the HuggingFace Inc. team.
-# Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
+# Copyright (c) 2023, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
