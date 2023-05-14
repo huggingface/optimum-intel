4295bab964adfe569f2005744a6bad25a70dbe8d
Helena Kloosterman <helena.kloosterman@intel.com>
2023-05-14 22:38:16 +0200

    Add accelerate requirement to neural-compressor and nncf extras
    PyTorch 4.29 raises this error:  ImportError: Using the `Trainer` with `PyTorch` requires `accelerate`: Run `pip install --upgrade accelerate`

--- setup.py
+++ setup.py
@@ -30,9 +30,10 @@ EXTRAS_REQUIRE = {
         "onnxruntime",
         "torch<2.0.0",  # remove after neural-compressor next release
         "intel-extension-for-pytorch<2.0.0",
+        "accelerate"
     ],
     "openvino": ["openvino>=2023.0.0.dev20230217", "onnx", "onnxruntime", "transformers<4.29"],
-    "nncf": ["nncf>=2.4.0", "openvino-dev>=2023.0.0.dev20230217"],
+    "nncf": ["nncf>=2.4.0", "openvino-dev>=2023.0.0.dev20230217", "accelerate"],
     "ipex": ["intel-extension-for-pytorch", "onnx"],
     "diffusers": ["diffusers"],
     "quality": QUALITY_REQUIRE,
