f10eef0ba22e062d8d0a606da39a57f9751f8ba7
Abhishek C.V. Salian <43337429+AbhishekSalian@users.noreply.github.com>
2023-06-05 13:48:41 +0530

    Fix INC trainer generation evaluation (#335)

--- optimum/intel/neural_compressor/trainer_seq2seq.py
+++ optimum/intel/neural_compressor/trainer_seq2seq.py
@@ -157,8 +157,8 @@ class INCSeq2SeqTrainer(INCTrainer):
 
         # XXX: adapt synced_gpus for fairscale as well
         gen_kwargs = {
-            "max_length": self._max_length if self._max_length is not None else self.config.max_length,
-            "num_beams": self._num_beams if self._num_beams is not None else self.config.num_beams,
+            "max_length": self._max_length if self._max_length is not None else self.model.config.max_length,
+            "num_beams": self._num_beams if self._num_beams is not None else self.model.config.num_beams,
             "synced_gpus": True if is_deepspeed_zero3_enabled() else False,
         }
 
