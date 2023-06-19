f74426cd62b0d74c3baf32971c519f0686b2cc50
Alexander <kozzzloff@list.ru>
2023-06-19 12:52:34 +0400

    Style

--- optimum/intel/openvino/quantization.py
+++ optimum/intel/openvino/quantization.py
@@ -18,10 +18,9 @@ import logging
 from copy import deepcopy
 from itertools import chain
 from pathlib import Path
-from typing import Callable, Dict, Optional, Tuple, Union, Any
+from typing import Any, Callable, Dict, Optional, Tuple, Union
 
 import nncf
-import numpy as np
 import openvino
 import torch
 import transformers
@@ -238,13 +237,13 @@ class OVQuantizer(OptimumQuantizer):
             def __call__(self, *args, **kwargs):
                 data_cache.append(deepcopy(*args))
                 return self.request(*args, *kwargs)
-            
+
             def start_async(
-                            self,
-                            inputs: Any = None,
-                            userdata: Any = None,
-                            shared_memory: bool = False,
-                        ) -> None:             
+                self,
+                inputs: Any = None,
+                userdata: Any = None,
+                shared_memory: bool = False,
+            ) -> None:
                 data_cache.append(inputs)
                 self.request.start_async(inputs, userdata, shared_memory)
 
