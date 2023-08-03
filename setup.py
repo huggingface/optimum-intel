4d8628465c9ddb9bde0fb23d9b19ced10faef499
Alexander <kozzzloff@list.ru>
2023-08-03 11:12:43 +0400

    Changed NNCF version to develop

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ EXTRAS_REQUIRE = {
         "onnxruntime<1.15.0",
     ],
     "openvino": ["openvino==2023.1.0.dev20230728", "onnx", "onnxruntime"],
-    "nncf": ["nncf>=2.5.0"],
+    "nncf": ["git+https://github.com/openvinotoolkit/nncf.git"],
     "ipex": ["transformers<4.32.0", "intel-extension-for-pytorch", "onnx"],
     "diffusers": ["diffusers", "invisible-watermark>=0.2.0"],
     "quality": QUALITY_REQUIRE,
