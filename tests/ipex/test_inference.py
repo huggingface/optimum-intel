d3e38908d3d8d1f452206d5bb9bd6ed5f39619bb
changwangss <chang1.wang@intel.com>
2023-09-25 18:35:52 -0700

    add gptbigcode to ipex test
    Signed-off-by: changwangss <chang1.wang@intel.com>

--- tests/ipex/test_inference.py
+++ tests/ipex/test_inference.py
@@ -40,6 +40,7 @@ MODEL_NAMES = {
     "gpt2": "hf-internal-testing/tiny-random-gpt2",
     "gpt_neo": "hf-internal-testing/tiny-random-GPTNeoModel",
     "gpt_neox": "hf-internal-testing/tiny-random-GPTNeoXForCausalLM",
+    "gpt_bigcode": "hf-internal-testing/tiny-random-GPTBigCodeModel",
 }
 
 _CLASSIFICATION_TASK_TO_AUTOMODELS = {
@@ -59,6 +60,7 @@ class IPEXIntegrationTest(unittest.TestCase):
         "gptj",
         "gpt2",
         "gpt_neo",
+        "gpt_bigcode"
     )
 
     QA_SUPPORTED_ARCHITECTURES = (
