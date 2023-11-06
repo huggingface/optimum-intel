398aaa9dd2eca0c685eabd731bbd1749e47b9ce9
Ella Charlaix <ella@huggingface.co>
2023-11-06 13:55:31 +0100

    fix transformers version

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ EXTRAS_REQUIRE = {
         "onnxruntime<1.15.0",
         "transformers>=4.33.0",
     ],
-    "openvino": ["openvino>=2023.1.0", "onnx", "onnxruntime", "transformers>=4.33.0"],
+    "openvino": ["openvino>=2023.1.0", "onnx", "onnxruntime", "transformers>=4.33.0,<4.35.0"],
     "nncf": ["nncf>=2.6.0"],
     "ipex": ["transformers<4.32.0", "intel-extension-for-pytorch", "onnx"],
     "diffusers": ["diffusers"],
