892374d331c8e3120978bca46b51061335eff9e4
Ella Charlaix <ella@huggingface.co>
2023-11-06 18:03:58 +0100

    fix version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except Exception as error:
     assert False, "Error: Could not open '%s' due %s\n" % (filepath, error)
 
 INSTALL_REQUIRE = [
-    "optimum @ git+https://github.com/huggingface/optimum.git",
+    "optimum>=1.14.0",
     "transformers>=4.20.0",
     "datasets>=1.4.0",
     "sentencepiece",
