03a6fcc4d7fd23e335a89ff18ef03c64409eb3ab
Ella Charlaix <ella@huggingface.co>
2024-02-15 11:31:37 +0100

    check tests

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except Exception as error:
 
 INSTALL_REQUIRE = [
     "torch>=1.11",
-    "optimum>=1.15.0",  # TODO : 1.17.0
+    "optimum @ git+https://github.com/huggingface/optimum.git",  # TODO : 1.17.0
     "transformers>=4.26.0",
     "datasets>=1.4.0",
     "sentencepiece",
