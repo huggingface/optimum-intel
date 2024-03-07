429e34cb2377ea5e2d750ed03537717b0e20d52a
Ella Charlaix <ella@huggingface.co>
2024-03-07 10:48:47 +0100

    add accelerate dependency for inc extra

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ TESTS_REQUIRE = [
 QUALITY_REQUIRE = ["black~=23.1", "ruff>=0.0.241"]
 
 EXTRAS_REQUIRE = {
-    "neural-compressor": ["neural-compressor>=2.2.0", "onnx", "onnxruntime<1.15.0"],
+    "neural-compressor": ["neural-compressor>=2.2.0", "onnx", "onnxruntime<1.15.0", "accelerate"],
     "openvino": ["openvino>=2023.3", "onnx", "nncf>=2.8.1"],
     "openvino-tokenizers": ["openvino-tokenizers[transformers]"],
     "nncf": ["nncf>=2.8.1"],
