59ce8a7165097a96a2fd245ef1126851d424609b
Ella Charlaix <ella@huggingface.co>
2024-03-28 19:44:18 +0100

    add missing import

--- optimum/intel/neural_compressor/configuration.py
+++ optimum/intel/neural_compressor/configuration.py
@@ -12,6 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+from dataclasses import dataclass
 from typing import Dict, Optional, Union
 
 from neural_compressor.config import DistillationConfig, WeightPruningConfig, _BaseQuantizationConfig
