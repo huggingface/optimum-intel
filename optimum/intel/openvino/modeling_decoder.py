cdbedb42aad95116743241fd7142bdc1a6101566
Nikita Savelyev <nikita.savelyev@intel.com>
2024-04-23 15:52:27 +0200

    ruff

--- optimum/intel/openvino/modeling_decoder.py
+++ optimum/intel/openvino/modeling_decoder.py
@@ -23,7 +23,7 @@ import openvino
 import torch
 from openvino.preprocess import PrePostProcessor
 from openvino.runtime import Core, Tensor, Type
-from transformers import AutoModelForCausalLM, AutoTokenizer, PretrainedConfig
+from transformers import AutoModelForCausalLM, PretrainedConfig
 from transformers.file_utils import add_start_docstrings, add_start_docstrings_to_model_forward
 from transformers.generation import GenerationMixin
 from transformers.modeling_outputs import CausalLMOutputWithPast
