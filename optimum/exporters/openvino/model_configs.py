5a786a575c7ab0b7a4df082a3ae681e8e7a4f535
eaidova <ekaterina.aidova@intel.com>
2024-04-24 20:31:24 +0400

    add test model

--- optimum/exporters/openvino/model_configs.py
+++ optimum/exporters/openvino/model_configs.py
@@ -460,12 +460,10 @@ class OVFalconDummyPastKeyValuesGenerator(FalconDummyPastKeyValuesGenerator):
             random_sequence_length_range=random_sequence_length_range,
             **kwargs,
         )
-        if normalized_config.new_decoder_architecture and normalized_config.multi_query:
+        if normalized_config.new_decoder_architecture:
             self.num_kv_heads = normalized_config.num_attention_heads
-        elif normalized_config.new_decoder_architecture and not normalized_config.multi_query:
-            self.num_kv_heads = normalized_config.num_kv_heads
         else:
-            self.num_kv_heads = 1
+            self.num_kv_heads = normalized_config.num_kv_heads if not normalized_config.multi_query else 1
 
         self.head_dim = self.hidden_size // self.num_attention_heads
 
