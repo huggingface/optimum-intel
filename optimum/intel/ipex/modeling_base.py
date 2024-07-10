e71f067ea5520708deb49b85f4b63fd4a7efe1d1
jiqing-feng <jiqing.feng@intel.com>
2024-07-09 20:48:43 -0400

    ,

--- optimum/intel/ipex/modeling_base.py
+++ optimum/intel/ipex/modeling_base.py
@@ -110,7 +110,9 @@ def ipex_jit_trace(model, task, use_cache):
             sample_inputs.pop("past_key_values")
 
     # Use Tensor Processing Primitives to accelerate linear, see https://arxiv.org/abs/2104.05755.
-    _enable_tpp()
+    # Only ipex >= 2.3.0 supports tpp.
+    if is_ipex_version(">=", "2.3.0"):
+        _enable_tpp()
     model = ipex.optimize(model.eval(), dtype=model.dtype, inplace=True)
     # Disable repack while jit tracing to reduce the memory
     ipex._C.disable_jit_linear_repack()
