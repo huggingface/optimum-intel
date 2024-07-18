51417a38b41c78549beb2877af4669b62e3c1b91
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2024-07-18 10:13:28 +0200

    fix _save_model call assesrtions

--- tests/openvino/test_quantization.py
+++ tests/openvino/test_quantization.py
@@ -519,7 +519,10 @@ class OVWeightCompressionTest(unittest.TestCase):
                         MODEL_NAMES["llama"], export=True, compile=False, use_cache=False
                     )
                     save_model_patch.assert_called_with(
-                        unittest.mock.ANY, unittest.mock.ANY, ov_config=OVConfig(quantization_config={"bits": 8})
+                        unittest.mock.ANY,
+                        unittest.mock.ANY,
+                        ov_config=OVConfig(quantization_config={"bits": 8}),
+                        library_name="transformers",
                     )
 
     def test_ovmodel_load_large_model_with_uncompressed_weights(self):
@@ -534,7 +537,10 @@ class OVWeightCompressionTest(unittest.TestCase):
                         MODEL_NAMES["llama"], export=True, load_in_8bit=False, compile=False, use_cache=False
                     )
                     save_model_patch.assert_called_with(
-                        unittest.mock.ANY, unittest.mock.ANY, ov_config=OVConfig(dtype="fp32")
+                        unittest.mock.ANY,
+                        unittest.mock.ANY,
+                        ov_config=OVConfig(dtype="fp32"),
+                        library_name="transformers",
                     )
 
     def test_ovmodel_load_large_model_with_additional_quantization_config(self):
@@ -554,7 +560,10 @@ class OVWeightCompressionTest(unittest.TestCase):
                         )
                         # quantization will be performed later, using load_model
                         save_model_patch.assert_called_with(
-                            unittest.mock.ANY, unittest.mock.ANY, ov_config=OVConfig(dtype="fp32")
+                            unittest.mock.ANY,
+                            unittest.mock.ANY,
+                            ov_config=OVConfig(dtype="fp32"),
+                            library_name="transformers",
                         )
                         compression_params = {
                             "mode": nncf.CompressWeightsMode.INT4_SYM,
