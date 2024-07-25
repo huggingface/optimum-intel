dd31f88b202439ec225fb5aa243af7997ed90134
Artur Paniukov <chgk1101@gmail.com>
2024-07-25 13:02:42 +0100

    Make Style

--- tests/openvino/test_exporters_cli.py
+++ tests/openvino/test_exporters_cli.py
@@ -172,8 +172,7 @@ class OVCLIExportTestCase(unittest.TestCase):
                 self.assertTrue("Detokenizer is not supported, convert tokenizer only." in output, output)
 
             if task.startswith("text-generation"):
-                self.assertTrue("Set padding side to left" in output, output)
-
+                self.assertFalse("Set padding side to left" in output, output)
 
     @parameterized.expand(SUPPORTED_ARCHITECTURES)
     def test_exporters_cli_fp16(self, task: str, model_type: str):
