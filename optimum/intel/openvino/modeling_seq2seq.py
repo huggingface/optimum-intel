99dc330691e21426145304caf488cb01177c583f
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2024-08-05 11:24:09 +0200

    fix

--- optimum/intel/openvino/modeling_seq2seq.py
+++ optimum/intel/openvino/modeling_seq2seq.py
@@ -344,10 +344,6 @@ class OVModelForSeq2SeqLM(OVBaseModelForSeq2SeqLM, GenerationMixin):
     def to(self, device: str):
         if isinstance(device, str):
             self._device = device.upper()
-            self.encoder._device = self._device
-            self.decoder._device = self._device
-            if self.use_cache:
-                self.decoder_with_past._device = self._device
             self.clear_requests()
         else:
             logger.debug(f"device must be of type {str} but got {type(device)} instead")
