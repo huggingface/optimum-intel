73afaa72bba268db6da6b534e26f9d9a82bd8b62
Andrei Anufriev <andrey.anufriev@intel.com>
2024-09-18 14:13:07 +0200

    Removed unverified parameters.

--- optimum/intel/openvino/configuration.py
+++ optimum/intel/openvino/configuration.py
@@ -164,22 +164,6 @@ _DEFAULT_4BIT_CONFIGS = {
         "dataset": "wikitext2",
         "scale_estimation": True,
     },
-    "meta-llama/Meta-Llama-3-8B-Instruct": {
-        "bits": 4,
-        "sym": False,
-        "group_size": 64,
-        "ratio": 0.9,
-        "dataset": "wikitext2",
-        "scale_estimation": True,
-    },
-    "meta-llama/Meta-Llama-3-8B": {
-        "bits": 4,
-        "sym": False,
-        "group_size": 64,
-        "ratio": 0.9,
-        "dataset": "wikitext2",
-        "scale_estimation": True,
-    },
     "meta-llama/Meta-Llama-3.1-8B-Instruct": {
         "bits": 4,
         "sym": False,
