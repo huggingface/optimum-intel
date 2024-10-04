3071edf3ff623f323a44ca669607fa79797835e2
Aidova <ekaterina.aidova@intel.com>
2024-10-04 08:49:12 +0400

    fix windows tmp dir issues

--- optimum/exporters/openvino/convert.py
+++ optimum/exporters/openvino/convert.py
@@ -62,6 +62,7 @@ from .utils import (
     _get_open_clip_submodels_fn_and_export_configs,
     clear_class_registry,
     remove_none_from_dummy_inputs,
+    calculate_model_size
 )
 
 
@@ -100,6 +101,7 @@ def export(
     stateful: bool = True,
     patch_16bit_model: bool = False,
     library_name: Optional[str] = None,
+    return_model_size: bool = False,
 ) -> Tuple[List[str], List[str]]:
     """
     Exports a Pytorch or TensorFlow model to an OpenVINO Intermediate Representation.
@@ -153,6 +155,7 @@ def export(
             stateful=stateful,
             patch_16bit_model=patch_16bit_model,
             library_name=library_name,
+            return_model_size=return_model_size
         )
 
     elif is_tf_available() and issubclass(type(model), TFPreTrainedModel):
@@ -163,7 +166,7 @@ def export(
             raise RuntimeError("`tf2onnx` does not support export on CUDA device.")
         if input_shapes is not None:
             logger.info("`input_shapes` argument is not supported by the Tensorflow ONNX export and will be ignored.")
-        return export_tensorflow(model, config, opset, output, ov_config=ov_config, library_name=library_name)
+        return export_tensorflow(model, config, opset, output, ov_config=ov_config, return_model_size=return_model_size, library_name=library_name)
 
     else:
         raise RuntimeError(
@@ -178,6 +181,7 @@ def export_tensorflow(
     output: Path,
     ov_config: Optional["OVConfig"] = None,
     library_name: Optional[str] = None,
+    return_model_size: bool = False
 ):
     """
     Export the TensorFlow model to OpenVINO format.
@@ -199,13 +203,17 @@ def export_tensorflow(
 
     library_name = _infer_library_from_model_or_model_class(model=model, library_name=library_name)
 
+    model_size = None
+    if return_model_size:
+        model_size = calculate_model_size(ov_model)
+
     _save_model(
         ov_model,
         output.parent / output,
         ov_config=ov_config,
         library_name=library_name,
     )
-    return input_names, output_names, True
+    return (input_names, output_names, True), model_size
 
 
 def export_pytorch_via_onnx(
@@ -218,6 +226,7 @@ def export_pytorch_via_onnx(
     model_kwargs: Optional[Dict[str, Any]] = None,
     ov_config: Optional["OVConfig"] = None,
     library_name: Optional[str] = None,
+    return_model_size: bool = False
 ):
     """
     Exports a PyTorch model to an OpenVINO Intermediate Representation via ONNX export.
@@ -261,13 +270,17 @@ def export_pytorch_via_onnx(
 
     library_name = _infer_library_from_model_or_model_class(model=model, library_name=library_name)
 
+
+    model_size = None
+    if return_model_size:
+        model_size = calculate_model_size(ov_model)
     _save_model(
         ov_model,
         output.parent / OV_XML_FILE_NAME if output.suffix != ".xml" else output,
         ov_config=ov_config,
         library_name=library_name,
     )
-    return input_names, output_names, True
+    return (input_names, output_names, True), model_size
 
 
 def export_pytorch(
@@ -282,6 +295,7 @@ def export_pytorch(
     stateful: bool = False,
     patch_16bit_model: bool = False,
     library_name: Optional[str] = None,
+    return_model_size: bool = False
 ) -> Tuple[List[str], List[str]]:
     """
     Exports a PyTorch model to an OpenVINO Intermediate Representation.
@@ -415,6 +429,7 @@ def export_pytorch(
                 model_kwargs,
                 ov_config=ov_config,
                 library_name=library_name,
+                return_model_size=return_model_size
             )
 
         ov_model.validate_nodes_and_infer_types()  # TODO: remove as unnecessary validation?
@@ -433,17 +448,21 @@ def export_pytorch(
             patch_stateful(model.config, ov_model)
 
         library_name = _infer_library_from_model_or_model_class(model=model, library_name=library_name)
+        model_size = None
 
+        if return_model_size:
+           model_size = calculate_model_size(ov_model)
         _save_model(
             ov_model,
             output,
             ov_config=ov_config,
             library_name=library_name,
         )
+        del ov_model
         clear_class_registry()
         del model
         gc.collect()
-    return input_names, output_names, False
+    return (input_names, output_names, False), model_size
 
 
 def export_models(
@@ -460,6 +479,7 @@ def export_models(
     stateful: bool = True,
     patch_16bit_model: bool = False,
     library_name: Optional[str] = None,
+    return_model_sizes:bool = False
 ) -> Tuple[List[List[str]], List[List[str]]]:
     """
     Export the models to OpenVINO IR format
@@ -489,7 +509,7 @@ def export_models(
     """
 
     outputs = []
-
+    model_sizes = []
     if output_names is not None and len(output_names) != len(models_and_export_configs):
         raise ValueError(
             f"Provided custom names {output_names} for the export of {len(models_and_export_configs)} models. Please provide the same number of names as models to export."
@@ -500,8 +520,7 @@ def export_models(
         output_name = output_names[i] if output_names is not None else Path(model_name + ".xml")
         output_path = output_dir / output_name
         output_path.parent.mkdir(parents=True, exist_ok=True)
-        outputs.append(
-            export(
+        model_outputs, model_size = export(
                 model=submodel,
                 config=sub_export_config,
                 output=output_path,
@@ -513,11 +532,12 @@ def export_models(
                 stateful=stateful[i] if isinstance(stateful, (list, tuple)) else stateful,
                 patch_16bit_model=patch_16bit_model,
                 library_name=library_name,
+                return_model_size=return_model_sizes,
             )
-        )
-
+        outputs.append(model_outputs)
+        model_sizes.append(model_size)
     outputs = list(map(list, zip(*outputs)))
-    return outputs
+    return outputs, model_sizes
 
 
 def export_from_model(
@@ -534,6 +554,7 @@ def export_from_model(
     device: str = "cpu",
     trust_remote_code: bool = False,
     patch_16bit_model: bool = False,
+    return_model_sizes: bool = False,
     **kwargs_shapes,
 ):
     model_kwargs = model_kwargs or {}
@@ -698,7 +719,7 @@ def export_from_model(
 
         model.save_config(output)
 
-    export_models(
+    _, model_sizes = export_models(
         models_and_export_configs=models_and_export_configs,
         output_dir=output,
         output_names=files_subpaths,
@@ -710,9 +731,10 @@ def export_from_model(
         model_kwargs=model_kwargs,
         patch_16bit_model=patch_16bit_model,
         library_name=library_name,
+        return_model_sizes=return_model_sizes
     )
 
-    return files_subpaths
+    return files_subpaths, model_sizes
 
 
 def export_tokenizer(
--- optimum/exporters/openvino/utils.py
+++ optimum/exporters/openvino/utils.py
@@ -15,10 +15,12 @@
 import inspect
 from collections import namedtuple
 from typing import Any, Callable, Dict, List, Optional, Tuple, Union
+import operator
+from functools import reduce
 
 from transformers.utils import is_torch_available
 
-from openvino.runtime import Dimension, PartialShape, Symbol
+from openvino.runtime import Dimension, PartialShape, Symbol, Model, Type
 from openvino.runtime.utils.types import get_element_type
 from optimum.exporters import TasksManager
 from optimum.exporters.onnx.base import OnnxConfig
@@ -209,3 +211,11 @@ def _get_open_clip_submodels_fn_and_export_configs(
 
 
 MULTI_MODAL_TEXT_GENERATION_MODELS = ["llava", "llava-next", "internvl-chat"]
+
+
+def calculate_model_size(ov_model: Model):
+    num_parameters = 0
+    for op in ov_model.get_ops():
+        if op.get_type_name() == "Constant" and op.get_element_type() in [Type.f16, Type.f32, Type.bf16]:
+            num_parameters += reduce(operator.mul, op.shape, 1)
+    return num_parameters
