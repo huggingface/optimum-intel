542347b07eaae93b89fdb8df962e02d7f114f85d
Ella Charlaix <80481427+echarlaix@users.noreply.github.com>
2024-10-07 22:26:32 +0200

    Install optimum from source (#926)
    * install optimum from source
    
    * add generator
    
    * fix

--- tests/openvino/test_modeling.py
+++ tests/openvino/test_modeling.py
@@ -265,7 +265,6 @@ class OVModelIntegrationTest(unittest.TestCase):
         loaded_pipeline.compile()
         self.assertEqual(loaded_pipeline.unet.request.get_property("PERFORMANCE_HINT"), "LATENCY")
         batch_size, height, width = 2, 16, 16
-        np.random.seed(0)
         inputs = {
             "prompt": ["sailing ship in storm by Leonardo da Vinci"] * batch_size,
             "height": height,
@@ -273,7 +272,7 @@ class OVModelIntegrationTest(unittest.TestCase):
             "num_inference_steps": 2,
             "output_type": "np",
         }
-        pipeline_outputs = loaded_pipeline(**inputs).images
+        pipeline_outputs = loaded_pipeline(**inputs, generator=np.random.RandomState(SEED)).images
         self.assertEqual(pipeline_outputs.shape, (batch_size, height, width, 3))
         with tempfile.TemporaryDirectory() as tmpdirname:
             loaded_pipeline.save_pretrained(tmpdirname)
@@ -295,13 +294,11 @@ class OVModelIntegrationTest(unittest.TestCase):
             self.assertIsInstance(compile_only_pipeline.text_encoder.model, ov.runtime.CompiledModel)
             self.assertIsInstance(compile_only_pipeline.vae_encoder.model, ov.runtime.CompiledModel)
             self.assertIsInstance(compile_only_pipeline.vae_decoder.model, ov.runtime.CompiledModel)
-            np.random.seed(0)
-            outputs = compile_only_pipeline(**inputs).images
+            outputs = compile_only_pipeline(**inputs, generator=np.random.RandomState(SEED)).images
             self.assertTrue(np.array_equal(pipeline_outputs, outputs))
             del compile_only_pipeline
 
-        np.random.seed(0)
-        outputs = pipeline(**inputs).images
+        outputs = pipeline(**inputs, generator=np.random.RandomState(SEED)).images
         self.assertTrue(np.array_equal(pipeline_outputs, outputs))
         del pipeline
         gc.collect()
