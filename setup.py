43052327af849661ef85f866a94c3084f0bba9e4
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2024-10-27 14:31:51 +0100

    fix

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ except Exception as error:
 
 INSTALL_REQUIRE = [
     "torch>=1.11",
-    "transformers>=4.36,<4.46",
+    "transformers>=4.36.0,<4.46.0",
     "optimum~=1.23",
     "datasets>=1.4.0",
     "sentencepiece",
