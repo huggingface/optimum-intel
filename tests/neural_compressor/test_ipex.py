912aefcdd54e8c0fa433c301056cb8b7971d9c69
jiqing-feng <jiqing.feng@intel.com>
2024-11-22 11:31:03 +0000

    fix inc tests
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>

--- tests/neural_compressor/test_ipex.py
+++ tests/neural_compressor/test_ipex.py
@@ -53,7 +53,6 @@ class IPEXQuantizationTest(INCTestMixin):
     SUPPORTED_ARCHITECTURES_WITH_EXPECTED_QUANTIZED_MATMULS = (("text-classification", "bert", 21),)
 
     @parameterized.expand(SUPPORTED_ARCHITECTURES_WITH_EXPECTED_QUANTIZED_MATMULS)
-    @unittest.skip(reason="INC is going to deprecate, skip this failed test")
     def test_ipex_static_quantization_with_smoothquant(self, task, model_arch, expected_quantized_matmuls):
         recipes = {"smooth_quant": True, "smooth_quant_args": {"alpha": 0.5}}
         num_samples = 10
@@ -81,5 +80,5 @@ class IPEXQuantizationTest(INCTestMixin):
                 is_static=True,
                 num_samples=num_samples,
                 load_inc_model=False,
-                load_ipex_model=True,
+                load_ipex_model=False,
             )
