5d2882b0fee63427de783ab9c84400294461c0f5
Nikita Savelyev <nikita.savelyev@intel.com>
2024-12-03 13:41:26 +0100

    Tweak test

--- tests/openvino/test_quantization.py
+++ tests/openvino/test_quantization.py
@@ -103,10 +103,9 @@ class OVQuantizerTest(unittest.TestCase):
                 processor=MODEL_NAMES["whisper"],
                 trust_remote_code=True,
                 weight_only=False,
-                smooth_quant_alpha=0.95,
             ),
-            (14, 22, 25),
-            (14, 22, 18),
+            (14, 21 if is_transformers_version("<=", "4.36.0") else 22, 25),
+            (14, 21 if is_transformers_version("<=", "4.36.0") else 22, 18),
         ),
     ]
 
