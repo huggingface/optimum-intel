3e2cf34e6a4bd8bee2d101cd2be522d644b894e3
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2024-12-20 12:13:28 +0100

    update optimum

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ except Exception as error:
 
 INSTALL_REQUIRE = [
     "torch>=1.11",
-    "optimum~=1.23",
+    "optimum@git+https://github.com/huggingface/optimum.git",
     "transformers>=4.36,<4.48",
     "datasets>=1.4.0",
     "sentencepiece",
