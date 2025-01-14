db98a90dc2810acb3d2fb70b36106be155c37068
Liu, Kaixuan <kaixuan.liu@intel.com>
2025-01-14 13:11:42 -0500

    upgrade transformers to 4.47 version; upgrade xpu to 2.5 version in Dockerfile
    Signed-off-by: Liu, Kaixuan <kaixuan.liu@intel.com>

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ EXTRAS_REQUIRE = {
     "nncf": ["nncf>=2.14.0"],
     "openvino": ["nncf>=2.14.0", "openvino>=2024.5.0", "openvino-tokenizers>=2024.5.0"],
     "neural-compressor": ["neural-compressor[pt]>3.0", "accelerate", "transformers<4.46"],
-    "ipex": ["intel-extension-for-pytorch>=2.4", "transformers>4.45,<4.47", "accelerate"],
+    "ipex": ["intel-extension-for-pytorch>=2.4", "transformers>4.45,<4.48", "accelerate"],
     "diffusers": ["diffusers"],
     "quality": QUALITY_REQUIRE,
     "tests": TESTS_REQUIRE,
