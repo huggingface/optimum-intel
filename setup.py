2e7c556256cd959bc92d6e01e7a7f29ae0aab92e
Ella Charlaix <80481427+echarlaix@users.noreply.github.com>
2025-01-30 17:16:01 +0100

    set optimum version to 1.24 (#1135)

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ except Exception as error:
 
 INSTALL_REQUIRE = [
     "torch>=1.11",
-    "optimum@git+https://github.com/huggingface/optimum.git",
+    "optimum~=1.24",
     "transformers>=4.36,<4.48",
     "datasets>=1.4.0",
     "sentencepiece",
