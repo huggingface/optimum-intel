8635b5caa7663b1618b404473d491ca27a4022ac
Ella Charlaix <80481427+echarlaix@users.noreply.github.com>
2025-02-17 14:06:32 +0100

    Trigger tests for maira2 for compatible transformers version only (#1161)

--- tests/openvino/test_modeling.py
+++ tests/openvino/test_modeling.py
@@ -2114,7 +2114,10 @@ class OVModelForVisualCausalLMIntegrationTest(unittest.TestCase):
     if is_transformers_version(">=", "4.40.0"):
         SUPPORTED_ARCHITECTURES += ["llava_next", "nanollava"]
     if is_transformers_version(">=", "4.45.0"):
-        SUPPORTED_ARCHITECTURES += ["minicpmv", "internvl2", "phi3_v", "qwen2_vl", "maira2"]
+        SUPPORTED_ARCHITECTURES += ["minicpmv", "internvl2", "phi3_v", "qwen2_vl"]
+    if is_transformers_version(">=", "4.46.0"):
+        SUPPORTED_ARCHITECTURES += ["maira2"]
+
     TASK = "image-text-to-text"
     REMOTE_CODE_MODELS = ["internvl2", "minicpmv", "nanollava", "phi3_v", "maira2"]
 
