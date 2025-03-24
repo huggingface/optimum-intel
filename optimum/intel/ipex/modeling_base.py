5513dd4abddb7ecb60f2b5f1493dfa4c56663d14
jiqing-feng <jiqing.feng@intel.com>
2025-03-24 09:55:10 +0000

    make compile as a static method
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>

--- optimum/intel/ipex/modeling_base.py
+++ optimum/intel/ipex/modeling_base.py
@@ -146,8 +146,7 @@ class IPEXModel(OptimizedModel):
         self.use_cache = kwargs.get("use_cache", False)
         self.model_save_dir = model_save_dir
         self._add_patch = _is_patched_with_ipex(model, self.export_feature, self.use_cache)
-        self.compile = self.can_compile()
-        model.config.compile = self.compile
+        self.model.config.compile = self.can_compile(self.model, self.use_cache)
 
         self.input_names = set(inspect.signature(model.forward).parameters)
 
@@ -159,10 +158,10 @@ class IPEXModel(OptimizedModel):
         if hasattr(self.auto_model_class, "register"):
             self.auto_model_class.register(AutoConfig, self.__class__)
 
-        if self.compile:
+        if getattr(self.model.config, "compile", False):
             self.apply_torch_compile()
 
-        if warmup and not self.compile:
+        if warmup and not getattr(self.model.config, "compile", False):
             self._init_warmup()
 
     @classmethod
@@ -233,15 +232,16 @@ class IPEXModel(OptimizedModel):
     def can_generate(self):
         return isinstance(self, GenerationMixin)
 
-    def can_compile(self):
+    @staticmethod
+    def can_compile(model, use_cache):
         if (
-            self.model.device.type != "cpu"
-            or self.config.model_type in _COMPILE_NOT_READY_MODEL_TYPES
+            model.device.type != "cpu"
+            or model.config.model_type in _COMPILE_NOT_READY_MODEL_TYPES
             or is_ipex_version("<", _IPEX_MINIMUM_VERSION_FOR_COMPILE)
-            or getattr(self.config, "quantization_config", None)
+            or getattr(model.config, "quantization_config", None)
         ):
             return False
-        if self.use_cache and not self._supports_static_cache:
+        if use_cache and not model._supports_static_cache:
             return False
 
         return True
@@ -333,7 +333,7 @@ class IPEXModelForCausalLM(IPEXModel, GenerationMixin):
         if hasattr(self.model_cls, "_convert_to_bloom_cache"):
             self._convert_to_bloom_cache = self.model_cls._convert_to_bloom_cache
 
-        if warmup and not self.compile:
+        if warmup and not getattr(self.model.config, "compile", False):
             self._init_warmup()
 
     @torch.no_grad()
@@ -353,7 +353,11 @@ class IPEXModelForCausalLM(IPEXModel, GenerationMixin):
         kwargs["use_cache"] = self.use_cache
         generation_config, model_kwargs = super()._prepare_generation_config(generation_config, **kwargs)
         generation_method = generation_config.get_generation_mode().value
-        if self.compile and generation_config.cache_implementation != "ipex_paged" and self._supports_static_cache:
+        if (
+            getattr(self.model.config, "compile", False)
+            and generation_config.cache_implementation != "ipex_paged"
+            and self._supports_static_cache
+        ):
             # Use static cache for torch compile
             generation_config.cache_implementation = "static"
         if generation_method not in _IPEX_EXPORTED_GENERATION_METHODS:
@@ -464,7 +468,7 @@ class IPEXModelForSeq2SeqLM(IPEXModel, GenerationMixin):
         if hasattr(self.model_cls, "_convert_to_standard_cache"):
             self._convert_to_standard_cache = self.model_cls._convert_to_standard_cache
 
-        if warmup and not self.compile:
+        if warmup and not getattr(self.model.config, "compile", False):
             self._init_warmup()
 
     @torch.no_grad()
@@ -481,7 +485,7 @@ class IPEXModelForSeq2SeqLM(IPEXModel, GenerationMixin):
     ) -> Tuple[GenerationConfig, Dict]:
         generation_config, model_kwargs = super()._prepare_generation_config(generation_config, **kwargs)
         # Use static cache for torch.compile
-        if self.compile:
+        if getattr(self.model.config, "compile", False):
             generation_config.cache_implementation = "static"
 
         return generation_config, model_kwargs
