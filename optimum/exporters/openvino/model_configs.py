f11b39a99108d4bfe7b96e55243dbffb0c24f190
eaidova <ekaterina.aidova@intel.com>
2025-04-08 10:41:42 +0400

    remove unused args

--- optimum/exporters/openvino/model_configs.py
+++ optimum/exporters/openvino/model_configs.py
@@ -2589,7 +2589,7 @@ class DummyPhi3VisionProjectionInputGenerator(DummyVisionInputGenerator):
         self._embed_layer_realization = normalized_config.config.embd_layer["embedding_cls"]
         self.image_dim_out = (
             normalized_config.config.img_processor.get(
-                "image_dim_out", normalized_config.config.img_processor["hidden_size"]
+                "image_dim_out", normalized_config.config.img_processor.get("hidden_size")
             )
             if normalized_config.config.img_processor is not None
             else 1152
