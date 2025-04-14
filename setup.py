e2cb3b9dd98a4a2ed3448af781a726800353fbd1
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2025-04-14 10:47:41 +0200

    snapshot downloads

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ except Exception as error:
 
 INSTALL_REQUIRE = [
     "torch>=1.11",
-    "optimum@git+https://github.com/huggingface/optimum.git@fix-ci",
+    "optimum@git+https://github.com/huggingface/optimum.git",
     "transformers>=4.36,<4.50",
     "datasets>=1.4.0",
     "sentencepiece",
