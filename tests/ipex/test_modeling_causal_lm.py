02649365b949e8d6bbb103107a64b39eb39f2a81
jiqing-feng <jiqing.feng@intel.com>
2025-04-25 14:14:16 +0000

    fix format
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>

--- tests/ipex/test_modeling_causal_lm.py
+++ tests/ipex/test_modeling_causal_lm.py
@@ -1,3 +1,17 @@
+#  Copyright 2025 The HuggingFace Team. All rights reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 import tempfile
 import unittest
 
@@ -16,6 +30,7 @@ from utils_tests import IS_XPU_AVAILABLE, MODEL_NAMES
 from optimum.intel import IPEXModelForCausalLM
 from optimum.intel.utils.import_utils import is_torch_version
 from optimum.utils.testing_utils import grid_parameters
+
 from .test_modeling import Timer
 
 
