3f858c9bf4ad4dc7071337aebcdeefbb63da9f63
changwangss <changwang@habana.ai>
2025-06-12 10:58:27 +0300

    fix inc version
    Signed-off-by: changwangss <changwang@habana.ai>

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ QUALITY_REQUIRE = ["black~=23.1", "ruff==0.4.4"]
 EXTRAS_REQUIRE = {
     "nncf": ["nncf>=2.16.0"],
     "openvino": ["nncf>=2.16.0", "openvino>=2025.1.0", "openvino-tokenizers>=2025.1.0"],
-    "neural-compressor": ["neural-compressor[pt]>3.0,<=3.3.1", "accelerate", "transformers<4.46"],
+    "neural-compressor": ["neural-compressor[pt]>3.0,!=3.4.0", "accelerate", "transformers<4.46"],
     "ipex": ["intel-extension-for-pytorch>=2.6", "transformers>4.49,<4.52", "accelerate"],
     "diffusers": ["diffusers"],
     "quality": QUALITY_REQUIRE,
