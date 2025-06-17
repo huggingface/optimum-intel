74e7cc3b382b9155c319810d3e855839fb74f0d9
jiqing-feng <jiqing.feng@intel.com>
2025-06-17 23:52:13 +0800

    Upgrade transformers version to 4.52 for ipex patching (#1317)
    * upgrade transformers version to 4.52 for ipex
    
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>
    
    * fix setup
    
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>
    
    * fix setup to support transformers 4.52.2
    
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>
    
    * fix head size check
    
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>
    
    * add comment for autoawq tests
    
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>
    
    * add minimal transformers version test
    
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>
    
    * add min transformers version
    
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>
    
    * update setup
    
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>
    
    * update transformers to 4.52
    
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>
    
    * general optimize: compute causal mask only when needed
    
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>
    
    * Update setup.py
    
    ---------
    
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>
    Co-authored-by: Ella Charlaix <80481427+echarlaix@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ except Exception as error:
 INSTALL_REQUIRE = [
     "torch>=1.11",
     "optimum==1.25.*",
-    "transformers>=4.36,<4.52",
+    "transformers>=4.36,<4.53",
     "datasets>=1.4.0",
     "setuptools",
     "scipy",
@@ -66,9 +66,9 @@ QUALITY_REQUIRE = ["black~=23.1", "ruff==0.4.4"]
 
 EXTRAS_REQUIRE = {
     "nncf": ["nncf>=2.16.0"],
-    "openvino": ["nncf>=2.16.0", "openvino>=2025.1.0", "openvino-tokenizers>=2025.1.0"],
+    "openvino": ["nncf>=2.16.0", "openvino>=2025.1.0", "openvino-tokenizers>=2025.1.0", "transformers<4.52"],
     "neural-compressor": ["neural-compressor[pt]>=3.4.1", "accelerate", "transformers<4.46"],
-    "ipex": ["intel-extension-for-pytorch>=2.6", "transformers>4.49,<4.52", "accelerate"],
+    "ipex": ["intel-extension-for-pytorch>=2.6", "transformers>4.50,<4.53", "accelerate"],
     "diffusers": ["diffusers"],
     "quality": QUALITY_REQUIRE,
     "tests": TESTS_REQUIRE,
