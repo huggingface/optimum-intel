bccfdc693cc1664b8ac6fd2eb4f84f403c0048d8
Ilyas Moutawwakil <57442720+IlyasMoutawwakil@users.noreply.github.com>
2025-07-10 18:58:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ except Exception as error:
 
 INSTALL_REQUIRE = [
     "torch>=1.11",
-    "optimum@git+https://github.com/huggingface/optimum.git",
+    "optimum@git+https://github.com/huggingface/optimum.git@transformers-4.53",
     "transformers>=4.36,<4.54",
     "datasets>=1.4.0",
     "setuptools",
