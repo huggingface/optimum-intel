d5ecc3778a438dc1527f2e48c4b1e9e245f49fd5
Nikita Savelyev <nikita.savelyev@intel.com>
2025-08-06 11:56:50 +0200

    Changes to mme_eval.py

--- mme_eval.py
+++ mme_eval.py
@@ -1,7 +1,7 @@
-import os
+from pathlib import Path
+
 from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
 
-import os
 import torch
 import string
 from datasets import load_dataset
@@ -65,45 +65,42 @@ class calculate_metrics:
         }
 
 
-def evaluate(model_path: str, model_id: str, category: str, max_new_tokens: int = 512):
+def evaluate(model_path: str, model_id: str, category: str, max_new_tokens: int = 32, limit=None):
     dataset = load_dataset("darkyarding/MME", split="test")
     if category != "all":
         dataset = dataset.filter(lambda x: x["category"] == category)
 
-    processor = AutoProcessor.from_pretrained(model_id, trust_remote_code=True)
-    # model_cls = get_model_class(model_id)
     model_cls = OVModelForVisualCausalLM
 
     model = model_cls.from_pretrained(
         model_path,
         trust_remote_code=True,
-        # torch_dtype=torch.bfloat16,
-        # device_map="auto",
-        # token=os.environ.get("HF_TOKEN", None),
         temperature=None,
         top_p=None,
         top_k=None,
+        # ov_config={"INFERENCE_PRECISION_HINT": "f32"}
     ).eval()
 
     try:
-        processor = AutoProcessor.from_pretrained(args.model_id, trust_remote_code=True)
+        processor = AutoProcessor.from_pretrained(model_id, trust_remote_code=True)
     except:
         processor = None
     try:
-        tokenizer = AutoTokenizer.from_pretrained(args.model_id, trust_remote_code=True)
+        tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
     except:
         tokenizer = None
     preprocess_kwargs = {}
-    if "internvl2" in args.model_id.lower():
-        preprocess_kwargs = {"config": AutoConfig.from_pretrained(args.model_id, trust_remote_code=True)}
+    if "internvl2" in model_id.lower():
+        preprocess_kwargs = {"config": AutoConfig.from_pretrained(model_id, trust_remote_code=True)}
 
     metric_util = calculate_metrics()
+
+    if limit is not None:
+        dataset = dataset.take(limit)
 
     all_items = []
     with torch.no_grad():
         for i, example in enumerate(tqdm(dataset)):
-            # if i == 10:
-            #     break
             prompt = example["question"]
             answer = example["answer"].strip().lower()
             image = example["image"].convert("RGB")
@@ -114,7 +111,7 @@ def evaluate(model_path: str, model_id: str, category: str, max_new_tokens: int 
 
             generate_ids = model.generate(
                 **inputs,
-                max_new_tokens=32,
+                max_new_tokens=max_new_tokens,
                 do_sample=False,
             )
             generate_ids = generate_ids[:, inputs['input_ids'].shape[1]:]
@@ -151,12 +148,68 @@ def evaluate(model_path: str, model_id: str, category: str, max_new_tokens: int 
     metrics["acc_plus"] = acc_plus
 
     print(f"\n MME Evaluation for '{category}'")
-    for k, v in metrics.items():
-        print(f"{k:>12}: {v:.4f}" if isinstance(v, float) else f"{k:>12}: {v}")
+    with open(f"{model_path}/mme_eval_{category}.txt", "w") as f:
+        f.write(f"\n MME Evaluation for '{category}'\n")
+        for k, v in metrics.items():
+            print(f"{k:>12}: {v:.4f}" if isinstance(v, float) else f"{k:>12}: {v}")
+            f.write(f"{k:>12}: {v:.4f}\n" if isinstance(v, float) else f"{k:>12}: {v}\n")
+    return metrics["acc"]
 
+
+def evaluate_on_many():
+    model_ids = [
+        "OpenGVLab/InternVL2-1B",
+        "OpenGVLab/InternVL2-4B",
+        "OpenGVLab/InternVL2_5-8B",
+        "microsoft/Phi-4-multimodal-instruct",
+        "microsoft/Phi-3.5-vision-instruct",
+        "Qwen/Qwen2-VL-7B-Instruct",
+        "Qwen/Qwen2.5-VL-3B-Instruct",
+        "llava-hf/LLaVA-NeXT-Video-7B-hf",
+        "llava-hf/llava-1.5-7b-hf",
+        "openbmb/MiniCPM-V-2_6",  # works on transformers 4.48
+    ]
+
+    models_dir = Path("/dev/data/nsavelye/workspace/models/vlm_ptq")
+    category = "scene"
+
+    accuracies = {}
+    for model_id in model_ids:
+        model_parent_path = models_dir / model_id.split("/")[-1]
+        for model_path in sorted(model_parent_path.glob("*/openvino_language_model.xml")):
+            model_path = model_path.parent
+            if (model_path / f"mme_eval_{category}.txt").exists():
+                print(f"Skipping {model_path} as it has already been evaluated.")
+                continue
+            print("\n" + '-'*100 + f"\nEvaluating {model_path}...")
+            try:
+                acc = evaluate(model_path=str(model_path), model_id=model_id, category=category)
+            except Exception as e:
+                # raise e
+                print(f"Error evaluating {model_path}: {e}")
+                continue
+            accuracies[str(model_path)] = acc
+            print(f"Finished evaluating {model_path}.")
+
+    for model_path, acc in accuracies.items():
+        print(f"{model_path}\taccuracy: {acc:.4f}")
+
+    for model_id in model_ids:
+        model_parent_path = models_dir / model_id.split("/")[-1]
+        for model_path in sorted(model_parent_path.glob("*/openvino_language_model.xml")):
+            model_path = model_path.parent
+            if not (model_path / f"mme_eval_{category}.txt").exists():
+                continue
+            with open(model_path / f"mme_eval_{category}.txt", "r") as f:
+                for line in f:
+                    if "acc" in line:
+                        print(model_path, line.strip())
+                        break
 
 if __name__ == "__main__":
     set_seed(42)
+
+    # evaluate_on_many()
 
     eval_type_dict = \
         ["existence", "count", "position", "color", "posters", "celebrity", "scene", "landmark", "artwork", "OCR"] + \
@@ -166,6 +219,7 @@ if __name__ == "__main__":
     parser.add_argument("--model-path", type=str, required=True, help="Path to ov model")
     parser.add_argument("--model-id", type=str, required=True, help="Huggingface model repo")
     parser.add_argument("--subset", choices=eval_type_dict, required=True, help="MME category name (e.g., 'Counting')")
+    parser.add_argument("--limit", type=int, help="Number of samples to evaluate on", default=None)
     args = parser.parse_args()
 
-    evaluate(model_id=args.model_id, model_path=args.model_path, category=args.subset)
+    evaluate(model_id=args.model_id, model_path=args.model_path, category=args.subset, limit=args.limit)
