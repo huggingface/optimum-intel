e40b683bb8b40b5afbc2fd247ace7b2a89cbf18b
Ella Charlaix <ella@huggingface.co>
2025-08-07 17:48:53 +0200

    add fix

--- optimum/intel/openvino/modeling_decoder.py
+++ optimum/intel/openvino/modeling_decoder.py
@@ -823,6 +823,15 @@ class OVModelForCausalLM(OVBaseDecoderModel, GenerationMixin):
                 )
             return tuple(np.take(past_state, beam_idx, 0) for past_state in past_key_values)
 
+
+    # modified from https://github.com/huggingface/transformers/blob/v4.55.0/src/transformers/generation/utils.py#L1992
+    def _prepare_cache_for_generation(self, *args, **kwargs):
+        """
+        This function is used to prepare the cache : when calling `generate` before the first inference, an instance of `DynamicCache` will be created.
+        For OVModel, we don't want model_kwargs to be updated before generation.
+        """
+        return
+
     @classmethod
     def _from_pretrained(
         cls,
