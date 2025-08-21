b6c80052d69ad822856f4bbb1e5182f168a5897d
Ella Charlaix <ella@huggingface.co>
2025-08-21 19:22:14 +0200

    fixe sdpa grouped query attention for llama4

--- optimum/exporters/openvino/model_patcher.py
+++ optimum/exporters/openvino/model_patcher.py
@@ -6338,6 +6338,60 @@ class Phi4MMVisionEmbeddingsPatcher(ModelPatcher):
         self._model.img_processor.embeddings.forward = self._model.img_processor.embeddings._orig_forward
 
 
+def sdpa_attention_forward_fixed(
+    module: torch.nn.Module,
+    query: torch.Tensor,
+    key: torch.Tensor,
+    value: torch.Tensor,
+    attention_mask: Optional[torch.Tensor],
+    dropout: float = 0.0,
+    scaling: Optional[float] = None,
+    is_causal: Optional[bool] = None,
+    **kwargs,
+) -> tuple[torch.Tensor, None]:
+    from transformers.integrations.sdpa_attention import repeat_kv
+
+    if kwargs.get("output_attentions", False) or kwargs.get("head_mask") is not None:
+        logger.warning_once(
+            "`sdpa` attention does not support `output_attentions=True` or `head_mask`."
+            " Please set your attention to `eager` if you want any of these features."
+        )
+    sdpa_kwargs = {}
+    if hasattr(module, "num_key_value_groups"):
+        key = repeat_kv(key, module.num_key_value_groups)
+        value = repeat_kv(value, module.num_key_value_groups)
+
+    if attention_mask is not None and attention_mask.ndim == 4:
+        attention_mask = attention_mask[:, :, :, : key.shape[-2]]
+
+    # We dispatch to SDPA's Flash Attention or Efficient kernels via this `is_causal` if statement instead of an inline conditional assignment
+    # in SDPA to support both torch.compile's dynamic shapes and full graph options. An inline conditional prevents dynamic shapes from compiling.
+    # Note that it is important to check first for the shape, otherwise compile will fail with `argument 'is_causal' must be bool, not SymBool`
+    if is_causal is None:
+        # The last condition is for encoder (decoder) models which specify this by passing their own `is_causal` flag
+        # This is mainly due to those models having mixed implementations for encoder, decoder, and encoder-decoder attns
+        is_causal = query.shape[2] > 1 and attention_mask is None and getattr(module, "is_causal", True)
+
+    # Shapes (e.g. query.shape[2]) are tensors during jit tracing, resulting in `is_causal` being a tensor.
+    # We convert it to a bool for the SDPA kernel that only accepts bools.
+    if torch.jit.is_tracing() and isinstance(is_causal, torch.Tensor):
+        is_causal = is_causal.item()
+
+    attn_output = torch.nn.functional.scaled_dot_product_attention(
+        query,
+        key,
+        value,
+        attn_mask=attention_mask,
+        dropout_p=dropout,
+        scale=scaling,
+        is_causal=is_causal,
+        **sdpa_kwargs,
+    )
+    attn_output = attn_output.transpose(1, 2).contiguous()
+
+    return attn_output, None
+
+
 class Llama4ImageEmbeddingsModelPatcher(ModelPatcher):
     def __init__(
         self,
@@ -6346,6 +6400,11 @@ class Llama4ImageEmbeddingsModelPatcher(ModelPatcher):
         model_kwargs: Dict[str, Any],
     ):
         model.__orig_forward = model.forward
+
+        if is_transformers_version(">=", "4.53"):
+            from transformers import AttentionInterface
+
+            AttentionInterface.register("sdpa", sdpa_attention_forward_fixed)
 
         # Adopted from https://github.com/huggingface/transformers/blob/v4.51.0/src/transformers/models/llama4/modeling_llama4.py#L1732-L1741
         def get_image_embeddings(self, pixel_values):
@@ -6363,6 +6422,13 @@ class Llama4ImageEmbeddingsModelPatcher(ModelPatcher):
 
     def __exit__(self, exc_type, exc_value, traceback):
         super().__exit__(exc_type, exc_value, traceback)
+
+        if is_transformers_version(">=", "4.53"):
+            from transformers import AttentionInterface
+            from transformers.integrations.sdpa_attention import sdpa_attention_forward
+
+            AttentionInterface.register("sdpa", sdpa_attention_forward)
+
         self._model.forward = self._model.__orig_forward
 
 
@@ -6462,6 +6528,7 @@ def llama4_attn_forward(
             attention_interface = eager_attention_forward
         else:
             attention_interface = ALL_ATTENTION_FUNCTIONS[self.config._attn_implementation]
+
     attn_output, attn_weights = attention_interface(
         self,
         query_states,
