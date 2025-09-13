b47cc60330f55e5e1d0290706048950ef9412f45
Ethan Yang <ethan.yang@intel.com>
2025-09-13 17:05:39 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,8 +28,8 @@ except Exception as error:
 
 INSTALL_REQUIRE = [
     "torch>=1.11",
-    "optimum==1.27.*",
-    "transformers>=4.36,<4.54",
+    "optimum==",
+    "transformers>=4.36",
     "datasets>=1.4.0",
     "setuptools",
     "scipy",
