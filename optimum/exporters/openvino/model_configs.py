d8736614b8b145abb799f8c48a1c690fa7d916a2
Roman Kazantsev <roman.kazantsev@intel.com>
2025-10-06 14:10:30 +0400

    Update optimum/exporters/openvino/model_configs.py

--- optimum/exporters/openvino/model_configs.py
+++ optimum/exporters/openvino/model_configs.py
@@ -2685,7 +2685,7 @@ class MiniCPMVOpenVINOConfig(BaseVLMOpenVINOConfig):
 @register_in_tasks_manager("minicpmo", *["image-text-to-text"], library_name="transformers")
 class MiniCPMOOpenVINOConfig(MiniCPMVOpenVINOConfig):
     MIN_TRANSFORMERS_VERSION = "4.43.0"
-    MAX_TRANSFORMERS_VERSION = "4.52.0"
+    MAX_TRANSFORMERS_VERSION = "4.51.99"
 
 
 class Phi3VisionConfigBehavior(str, enum.Enum):
