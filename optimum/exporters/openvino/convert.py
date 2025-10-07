6627c8008ddaf62371de099b94b6a3cd6d6a0d69
Ella Charlaix <80481427+echarlaix@users.noreply.github.com>
2025-10-07 16:32:50 +0200

    set MAX_TRANSFORMERS_VERSION as inclusive upper bound (#1463)
    * set MAX_TRANSFORMERS_VERSION as inclusive upper bound
    
    * set exact transformerrs version
    
    * clarify raised error message

--- optimum/exporters/openvino/convert.py
+++ optimum/exporters/openvino/convert.py
@@ -199,10 +199,10 @@ def export(
     if max_version is not None:
         if isinstance(max_version, Version):
             max_version = max_version.base_version
-        if is_transformers_version(">=", max_version):
+        if is_transformers_version(">", max_version):
             raise ValueError(
                 f"The current version of Transformers does not allow for the export of the model. Maximum required is "
-                f"{config.MAX_TRANSFORMERS_VERSION}, got: {_transformers_version}"
+                f"{config.MAX_TRANSFORMERS_VERSION.replace('99', '*')}, got: {_transformers_version}"
             )
 
     if stateful:
