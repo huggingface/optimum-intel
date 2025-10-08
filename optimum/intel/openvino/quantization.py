f9cff03a358280d59ee54fbf1739d5817b8f0797
Nikita Savelyev <nikita.savelyev@intel.com>
2025-10-08 14:20:24 +0200

    Fix VLM inplace static quantization (#1464)

--- optimum/intel/openvino/quantization.py
+++ optimum/intel/openvino/quantization.py
@@ -1440,6 +1440,15 @@ class OVQuantizer(OptimumQuantizer):
                     quantized_model = _mixed_quantization(submodel, config, nncf_dataset, **kwargs)
 
                 # Replace the original model with the quantized model
+                if isinstance(self.model, OVModelForVisualCausalLM):
+                    # Special handling of submodels in OVModelForVisualCausalLM
+                    # TODO (nikita.savelyevv): Implement a proper fix including other model types
+                    if submodel_name == "lm_model":
+                        self.model.language_model.model = quantized_model
+                    elif submodel_name == "text_embeddings_model":
+                        self.model.language_model.text_emb_model = quantized_model
+                    elif submodel_name == "vision_embeddings_model":
+                        self.model.vision_embeddings.model = quantized_model
                 if isinstance(getattr(self.model, submodel_name), openvino.Model):
                     setattr(self.model, submodel_name, quantized_model)
                 elif isinstance(getattr(getattr(self.model, submodel_name), "model"), openvino.Model):
