3160ce29d94f436b46d0c0e4a446681d326a9996
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2025-10-08 14:38:38 +0200

    style

--- tests/scripts/login_with_ci_token.py
+++ tests/scripts/login_with_ci_token.py
@@ -3,7 +3,7 @@ from huggingface_hub import login
 
 # fmt: off
 # not critical
-CI_TOKEN = "".join(['h', 'f', '_', 'X', 'N', 'C', 'O', 'g', 'A', 'O', 'g', 'S', 'H', 'W', 'Y', 'B', 'H', 'g', 'L', 'J', 'n', 'C', 'U', 'f', 'i', 'O', 'D', 'n', 'm', 'N', 'n', 'V', 'Q', 'Q', 'N', 'y', 'c'])  
+CI_TOKEN = "".join(['h', 'f', '_', 'X', 'N', 'C', 'O', 'g', 'A', 'O', 'g', 'S', 'H', 'W', 'Y', 'B', 'H', 'g', 'L', 'J', 'n', 'C', 'U', 'f', 'i', 'O', 'D', 'n', 'm', 'N', 'n', 'V', 'Q', 'Q', 'N', 'y', 'c'])
 # fmt: on
 
 login(token=CI_TOKEN)
