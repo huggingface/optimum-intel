0778f3779c5bb05a878f6d2ad5672f44b4eff4e0
Ella Charlaix <ella@huggingface.co>
2025-11-05 10:13:34 +0100

    add comment

--- optimum/exporters/openvino/__main__.py
+++ optimum/exporters/openvino/__main__.py
@@ -393,7 +393,8 @@ def main_export(
         if library_name == "open_clip":
             model = _OpenClipForZeroShotImageClassification.from_pretrained(model_name_or_path, cache_dir=cache_dir)
         else:
-
+            # remote code models like phi3_v internvl2, minicpmv, internvl2, nanollava, maira2 should be loaded using AutoModelForCausalLM and not AutoModelForImageTextToText
+            # TODO: use config.auto_map to load remote code models instead (for other models we can directly use config.architectures)
             task_model_loading = task
             if library_name == "transformers":
                 has_remote_code = hasattr(config, "auto_map")
