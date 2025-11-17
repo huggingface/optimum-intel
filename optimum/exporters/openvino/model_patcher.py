7e37aae5ad79c504ee66133945ce2dcf8587a36f
Kazantsev, Roman <roman.kazantsev@intel.com>
2025-11-17 15:21:14 +0400

    Extend patching

--- optimum/exporters/openvino/model_patcher.py
+++ optimum/exporters/openvino/model_patcher.py
@@ -6956,6 +6956,141 @@ class Zamba2ModelPatcher(ModelPatcher):
             mamba_layer.forward = mamba_layer._orig_forward
 
 
+def qwen3_next_gated_delta_net_forward(
+        self,
+        hidden_states: torch.Tensor,
+        cache_params = None,
+        cache_position: Optional[torch.LongTensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+):
+    def apply_mask_to_padding_states(hidden_states, attention_mask):
+        """
+        Tunes out the hidden states for padding tokens, see https://github.com/state-spaces/mamba/issues/66
+        """
+        # NOTE: attention mask is a 2D boolean tensor
+        if attention_mask is not None and attention_mask.shape[1] > 1 and attention_mask.shape[0] > 1:
+            dtype = hidden_states.dtype
+            hidden_states = (hidden_states * attention_mask[:, :, None]).to(dtype)
+
+        return hidden_states
+
+    hidden_states = apply_mask_to_padding_states(hidden_states, attention_mask)
+
+    # Set up dimensions for reshapes later
+    batch_size, seq_len, _ = hidden_states.shape
+
+    # distinguish prefill and decoding stage
+    dtype = hidden_states.dtype
+    # use_precomputed_states - is_decoding flag
+    use_precomputed_states = torch.tensor(seq_len == 1).to(dtype)
+
+    # getting projected states from cache if it exists
+    layer_idx = None
+    if cache_params is not None:
+        layer_idx = cache_params.linear_attn_mapping[self.layer_idx]
+        conv_state = cache_params.conv_states[layer_idx]
+        recurrent_state = cache_params.recurrent_states[layer_idx]
+
+    projected_states_qkvz = self.in_proj_qkvz(hidden_states)
+    projected_states_ba = self.in_proj_ba(hidden_states)
+    query, key, value, z, b, a = self.fix_query_key_value_ordering(projected_states_qkvz, projected_states_ba)
+    query, key, value = (x.reshape(x.shape[0], x.shape[1], -1) for x in (query, key, value))
+
+    mixed_qkv = torch.cat((query, key, value), dim=-1)
+    mixed_qkv = mixed_qkv.transpose(1, 2)
+
+    if cache_params is not None:
+        # 2. Convolution sequence transformation
+        # NOTE: the conv state is updated in `causal_conv1d_update`
+        conv_state_dec = conv_state.clone()
+        mixed_qkv_dec = mixed_qkv.clone()
+        mixed_qkv_dec = self.causal_conv1d_update(
+            mixed_qkv_dec,
+            conv_state_dec,
+            self.conv1d.weight.squeeze(1),
+            self.conv1d.bias,
+            self.activation,
+        )
+
+        conv_state_prefill = F.pad(mixed_qkv, (self.conv_kernel_size - mixed_qkv.shape[-1], 0))
+        conv_state = conv_state_dec * use_precomputed_states + conv_state_prefill * (1.0 - use_precomputed_states)
+        mixed_qkv = mixed_qkv_dec * use_precomputed_states + mixed_qkv * (1.0 - use_precomputed_states)
+        cache_params.conv_states[layer_idx] = conv_state
+
+        if self.causal_conv1d_fn is not None:
+            mixed_qkv = self.causal_conv1d_fn(
+                x=mixed_qkv,
+                weight=self.conv1d.weight.squeeze(1),
+                bias=self.conv1d.bias,
+                activation=self.activation,
+                seq_idx=None,
+            )
+    else:
+        mixed_qkv = F.silu(self.conv1d(mixed_qkv)[:, :, :seq_len])
+
+
+    mixed_qkv = mixed_qkv.transpose(1, 2)
+    query, key, value = torch.split(
+        mixed_qkv,
+        [
+            self.key_dim,
+            self.key_dim,
+            self.value_dim,
+        ],
+        dim=-1,
+    )
+    query = query.reshape(query.shape[0], query.shape[1], -1, self.head_k_dim)
+    key = key.reshape(key.shape[0], key.shape[1], -1, self.head_k_dim)
+    value = value.reshape(value.shape[0], value.shape[1], -1, self.head_v_dim)
+
+    beta = b.sigmoid()
+    # If the model is loaded in fp16, without the .float() here, A might be -inf
+    g = -self.A_log.float().exp() * F.softplus(a.float() + self.dt_bias)
+    if self.num_v_heads // self.num_k_heads > 1:
+        query = query.repeat_interleave(self.num_v_heads // self.num_k_heads, dim=2)
+        key = key.repeat_interleave(self.num_v_heads // self.num_k_heads, dim=2)
+
+    core_attn_out_prefill, last_recurrent_state_prefill = self.chunk_gated_delta_rule(
+        query,
+        key,
+        value,
+        g=g,
+        beta=beta,
+        initial_state=None,
+        output_final_state=cache_params is not None,
+        use_qk_l2norm_in_kernel=True,
+    )
+
+    core_attn_out_dec, last_recurrent_state_dec = self.recurrent_gated_delta_rule(
+        query,
+        key,
+        value,
+        g=g,
+        beta=beta,
+        initial_state=recurrent_state,
+        output_final_state=cache_params is not None,
+        use_qk_l2norm_in_kernel=True,
+    )
+
+    core_attn_out = core_attn_out_dec * use_precomputed_states + core_attn_out_prefill * (1.0 - use_precomputed_states)
+    last_recurrent_state = last_recurrent_state_dec * use_precomputed_states + last_recurrent_state_prefill * (1.0 - use_precomputed_states)
+
+    # Update cache
+    if cache_params is not None:
+        cache_params.recurrent_states[layer_idx] = last_recurrent_state
+
+    z_shape_og = z.shape
+    # reshape input data into 2D tensor
+    core_attn_out = core_attn_out.reshape(-1, core_attn_out.shape[-1])
+    z = z.reshape(-1, z.shape[-1])
+    core_attn_out = self.norm(core_attn_out, z)
+    core_attn_out = core_attn_out.reshape(z_shape_og)
+    core_attn_out = core_attn_out.reshape(core_attn_out.shape[0], core_attn_out.shape[1], -1)
+
+    output = self.out_proj(core_attn_out)
+    return output
+
+
 class Qwen3NextModelPatcher(ModelPatcher):
     def __init__(
         self,
@@ -7083,31 +7218,25 @@ class Qwen3NextModelPatcher(ModelPatcher):
         self.orig_forward = patched_forward
 
     def __enter__(self):
-        from transformers.models.zamba2.modeling_zamba2 import Zamba2HybridLayer, Zamba2MambaDecoderLayer
-
         super().__enter__()
         setattr(self._model, self.orig_forward_name, self.patched_forward)
 
-        #for layer in self._model.model.layers:
-        #    if isinstance(layer, Zamba2MambaDecoderLayer):
-        #        mamba_layer = layer.mamba
-        #    elif isinstance(layer, Zamba2HybridLayer):
-        #        mamba_layer = layer.mamba_decoder.mamba
-        #    else:
-        #        continue
-        #    mamba_layer._orig_forward = mamba_layer.forward
-        #    mamba_layer.forward = types.MethodType(zamba2_mamba_mixer, mamba_layer)
+        for idx, decoder_layer in enumerate(self._model.model.layers):
+            layer_type = self._model.model.config.layer_types[idx]
+            if layer_type == "linear_attention":
+                linear_attn_layer = decoder_layer.linear_attn
+            else:
+                continue
+            linear_attn_layer._orig_forward = linear_attn_layer.forward
+            linear_attn_layer.forward = types.MethodType(qwen3_next_gated_delta_net_forward, linear_attn_layer)
 
     def __exit__(self, exc_type, exc_value, traceback):
-        from transformers.models.zamba2.modeling_zamba2 import Zamba2HybridLayer, Zamba2MambaDecoderLayer
-
         super().__exit__(exc_type, exc_value, traceback)
         setattr(self._model, self.orig_forward_name, self.model_orig_forward)
-        #for layer in self._model.model.layers:
-        #    if isinstance(layer, Zamba2MambaDecoderLayer):
-        #        mamba_layer = layer.mamba
-        #    elif isinstance(layer, Zamba2HybridLayer):
-        #        mamba_layer = layer.mamba_decoder.mamba
-        #    else:
-        #        continue
-        #    mamba_layer.forward = mamba_layer._orig_forward
+        for idx, decoder_layer in enumerate(self._model.model.layers):
+            layer_type = self._model.model.config.layer_types[idx]
+            if layer_type == "linear_attention":
+                linear_attn_layer = decoder_layer.linear_attn
+            else:
+                continue
+            linear_attn_layer.forward = linear_attn_layer._orig_forward
