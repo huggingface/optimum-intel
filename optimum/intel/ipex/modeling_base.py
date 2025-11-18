e82cbadfcfb5965ba6a475e7abe1cc43eeecb522
Liu, Kaixuan <kaixuan.liu@intel.com>
2025-11-18 16:02:51 +0800

    fix bug for attention_mask when model is not patched
    Signed-off-by: Liu, Kaixuan <kaixuan.liu@intel.com>

--- optimum/intel/ipex/modeling_base.py
+++ optimum/intel/ipex/modeling_base.py
@@ -448,7 +448,8 @@ class IPEXModelForCausalLM(IPEXModel, GenerationMixin):
 
     def prepare_inputs_for_generation(self, *args, **kwargs):
         input_kwargs = self.model.prepare_inputs_for_generation(*args, **kwargs)
-        input_kwargs["attention_mask"] = kwargs.get("attention_mask", None)
+        if self.add_patch:
+            input_kwargs["attention_mask"] = kwargs.get("attention_mask", None)
         return input_kwargs
 
     def _supports_logits_to_keep(self) -> bool:
