0624368edff7869d405d569d482251054ef03726
Ethan Yang <ethan.yang@intel.com>
2025-12-03 10:24:18 +0800

    Update optimum/exporters/openvino/model_configs.py
    Co-authored-by: Ella Charlaix <80481427+echarlaix@users.noreply.github.com>

--- optimum/exporters/openvino/model_configs.py
+++ optimum/exporters/openvino/model_configs.py
@@ -4225,22 +4225,8 @@ class GPT2OpenVINOConfig(GPT2OnnxConfig):
 
 @register_in_tasks_manager("dinov3_vit", *["feature-extraction"], library_name="transformers")
 @register_in_tasks_manager("dinov3_convnext", *["feature-extraction"], library_name="transformers")
-class DinoV3OpenVINOConfig(VisionOnnxConfig):
+class DinoV3OpenVINOConfig(ViTOnnxConfig):
     MIN_TRANSFORMERS_VERSION = "4.56.0"
-    NORMALIZED_CONFIG_CLASS = NormalizedVisionConfig
-
-    @property
-    def inputs(self) -> Dict[str, Dict[int, str]]:
-        return {
-            "pixel_values": {0: "image_batch_size", 1: "num_channels", 2: "height", 3: "width"},
-        }
-
-    @property
-    def outputs(self) -> Dict[str, Dict[int, str]]:
-        return {
-            "last_hidden_state": {0: "image_batch_size"},
-            "pooler_output": {0: "image_batch_size"},
-        }
 
 
 @register_in_tasks_manager(
