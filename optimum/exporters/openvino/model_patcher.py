3af6853a13ea8fdcc012cdf1e8ef1e9122a4b0d0
Kazantsev, Roman <roman.kazantsev@intel.com>
2026-01-09 21:41:54 +0400

    Patch MoE with a vectorized form

--- optimum/exporters/openvino/model_patcher.py
+++ optimum/exporters/openvino/model_patcher.py
@@ -7411,39 +7411,63 @@ class GraniteMoeHybridModelPatcher(ModelPatcher):
             mamba_layer.forward = mamba_layer._orig_forward
 
 
+# Patch MoE implementation to enable correct Torch tracing:
+# https://huggingface.co/arcee-ai/Trinity-Nano-Preview/blob/main/modeling_afmoe.py#L265
+#
+# The original code contains a conditional branch inside a Python for-loop.
+# For certain example inputs, this branch may be skipped during tracing,
+# resulting in an incorrect or incomplete final graph.
+#
+# Additionally, the non-vectorized implementation produces a very large
+# OpenVINO graph with excessive nodes, which is expensive for graph
+# transformations and significantly increases model conversion time.
+# So the patch provides a vectorized form of MoE.
 def afmoe_moe_forward_patched(self, hidden_states):
+    num_experts = self.config.num_experts
     batch_size, seq_len, hidden_dim = hidden_states.shape
-    hidden_states_flat = hidden_states.view(-1, hidden_dim)
     routing_weights, selected_experts = self.router(hidden_states, self.expert_bias)
+    new_routing_weights = torch.zeros(batch_size * seq_len, self.config.num_experts, dtype=routing_weights.dtype)
+    new_routing_weights.scatter_(dim=1, index=selected_experts, src=routing_weights)
+    hidden_states = hidden_states.view(-1, hidden_dim)
 
     # Process through shared experts
     if self.shared_experts is not None:
-        shared_output = self.shared_experts(hidden_states_flat)
+        shared_output = self.shared_experts(hidden_states)
     else:
-        shared_output = torch.zeros_like(hidden_states_flat)
+        shared_output = torch.zeros_like(hidden_states)
 
-    final_hidden_states = torch.zeros_like(hidden_states_flat)
+    hidden_states = hidden_states.repeat(self.config.num_experts, 1)
+    hidden_states = hidden_states.view(self.config.num_experts, -1, hidden_dim)
+    act_fn = self.experts[0].act_fn
 
-    # One hot encode the selected experts to create an expert mask
-    # this will be used to easily index which expert is going to be solicited
-    expert_mask = torch.nn.functional.one_hot(selected_experts, num_classes=self.config.num_experts).permute(2, 1, 0)
+    # combine weights from all experts to get the common gate, up and down weights
+    down_projs = torch.concat(
+        tuple(self.experts[i].down_proj.weight.float().unsqueeze(0)
+              for i in range(num_experts)),
+        dim=0
+    ).transpose(1, 2)
+    gate_projs = torch.concat(
+        tuple(self.experts[i].gate_proj.weight.float().unsqueeze(0)
+              for i in range(num_experts)),
+        dim=0
+    ).transpose(1, 2)
+    up_projs = torch.concat(
+        tuple(self.experts[i].up_proj.weight.float().unsqueeze(0)
+              for i in range(num_experts)),
+        dim=0
+    ).transpose(1, 2)
 
-    # Loop over all available experts in the model and perform the computation on each expert
-    for expert_idx in range(self.config.num_experts):
-        expert_layer = self.experts[expert_idx]
-        idx, top_x = torch.where(expert_mask[expert_idx])
+    # compute experts outputs in a vectorized form
+    gate = torch.bmm(hidden_states, gate_projs)
+    up = torch.bmm(hidden_states, up_projs)
+    gate_up = act_fn(gate) * up
+    next_states = torch.bmm(gate_up, down_projs)
+    next_states = next_states.view(num_experts, batch_size, -1, hidden_dim)
+    next_states = next_states * new_routing_weights.transpose(0, 1).view(num_experts, batch_size, -1)[..., None]
+    next_states = next_states.sum(dim=0)
 
-        # Index the correct hidden states and compute the expert hidden state for
-        # the current expert. We need to make sure to multiply the output hidden
-        # states by `routing_weights` on the corresponding tokens (top-1 and top-2)
-        current_state = hidden_states_flat[None, top_x].reshape(-1, hidden_dim)
-        current_hidden_states = expert_layer(current_state) * routing_weights[top_x, idx, None]
-
-        # However `index_add_` only support torch tensors for indexing so we'll use
-        # the `top_x` tensor here.
-        final_hidden_states.index_add_(0, top_x, current_hidden_states.to(hidden_states_flat.dtype))
-
-    output = shared_output + final_hidden_states
+    shared_output = shared_output.view(batch_size, -1, hidden_dim)
+    output = shared_output + next_states
     return output.view(batch_size, seq_len, hidden_dim)
 
 
