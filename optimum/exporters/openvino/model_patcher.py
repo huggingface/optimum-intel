d8efcfcf72b0aa41349317f49180f144130a9e7d
Kazantsev, Roman <roman.kazantsev@intel.com>
2026-01-25 17:28:31 +0400

    Re-use make_causal_mask and expand_mask from transformers

--- optimum/exporters/openvino/model_patcher.py
+++ optimum/exporters/openvino/model_patcher.py
@@ -7822,37 +7822,6 @@ class LlamaEagle3PreTrainedModel(PreTrainedModel):
             module.weight.data.fill_(1.0)
 
 
-def _make_causal_mask(
-    input_ids_shape: torch.Size, dtype: torch.dtype, device: torch.device, past_key_values_length: int = 0
-):
-    """
-    Make causal mask used for bi-directional self-attention.
-    """
-    bsz, tgt_len = input_ids_shape
-    mask = torch.full((tgt_len, tgt_len), torch.finfo(dtype).min, device=device)
-    mask_cond = torch.arange(mask.size(-1), device=device)
-    mask.masked_fill_(mask_cond < (mask_cond + 1).view(mask.size(-1), 1), 0)
-    mask = mask.to(dtype)
-
-    if past_key_values_length > 0:
-        mask = torch.cat([torch.zeros(tgt_len, past_key_values_length, dtype=dtype, device=device), mask], dim=-1)
-    return mask[None, None, :, :].expand(bsz, 1, tgt_len, tgt_len + past_key_values_length)
-
-
-def _expand_mask(mask: torch.Tensor, dtype: torch.dtype, tgt_len: Optional[int] = None):
-    """
-    Expands attention_mask from `[bsz, seq_len]` to `[bsz, 1, tgt_seq_len, src_seq_len]`.
-    """
-    bsz, src_len = mask.size()
-    tgt_len = tgt_len if tgt_len is not None else src_len
-
-    expanded_mask = mask[:, None, None, :].expand(bsz, 1, tgt_len, src_len).to(dtype)
-
-    inverted_mask = 1.0 - expanded_mask
-
-    return inverted_mask.masked_fill(inverted_mask.to(torch.bool), torch.finfo(dtype).min)
-
-
 class LlamaEagle3Model(LlamaEagle3PreTrainedModel):
     def __init__(self, config: LlamaConfig):
         super().__init__(config)
@@ -7980,11 +7949,12 @@ class LlamaEagle3Model(LlamaEagle3PreTrainedModel):
         )
 
     def _prepare_decoder_attention_mask(self, attention_mask, input_shape, inputs_embeds, past_key_values_length):
+        from transformers.modeling_attn_mask_utils import AttentionMaskConverter
         # create causal mask
         # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
         combined_attention_mask = None
         if input_shape[-1] > 1:
-            combined_attention_mask = _make_causal_mask(
+            combined_attention_mask = AttentionMaskConverter._make_causal_mask(
                 input_shape,
                 # inputs_embeds.dtype,
                 torch.float32,  # [MODIFIED] force to cast to float32
@@ -7994,7 +7964,7 @@ class LlamaEagle3Model(LlamaEagle3PreTrainedModel):
 
         if attention_mask is not None:
             # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
-            expanded_attn_mask = _expand_mask(attention_mask, torch.float32, tgt_len=input_shape[-1]).to(
+            expanded_attn_mask = AttentionMaskConverter._expand_mask(attention_mask, torch.float32, tgt_len=input_shape[-1]).to(
                 inputs_embeds.device
             )
             combined_attention_mask = (
