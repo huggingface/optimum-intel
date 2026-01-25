bbda81734d6d90180998e13e7284bb4ce49c4a31
Kazantsev, Roman <roman.kazantsev@intel.com>
2026-01-25 17:30:54 +0400

    Apply formatting

--- optimum/exporters/openvino/model_patcher.py
+++ optimum/exporters/openvino/model_patcher.py
@@ -7950,6 +7950,7 @@ class LlamaEagle3Model(LlamaEagle3PreTrainedModel):
 
     def _prepare_decoder_attention_mask(self, attention_mask, input_shape, inputs_embeds, past_key_values_length):
         from transformers.modeling_attn_mask_utils import AttentionMaskConverter
+
         # create causal mask
         # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
         combined_attention_mask = None
@@ -7964,9 +7965,9 @@ class LlamaEagle3Model(LlamaEagle3PreTrainedModel):
 
         if attention_mask is not None:
             # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
-            expanded_attn_mask = AttentionMaskConverter._expand_mask(attention_mask, torch.float32, tgt_len=input_shape[-1]).to(
-                inputs_embeds.device
-            )
+            expanded_attn_mask = AttentionMaskConverter._expand_mask(
+                attention_mask, torch.float32, tgt_len=input_shape[-1]
+            ).to(inputs_embeds.device)
             combined_attention_mask = (
                 expanded_attn_mask if combined_attention_mask is None else expanded_attn_mask + combined_attention_mask
             )
