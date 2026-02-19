c7184e114939e777886cb1f2acb4b3abcca6f148
“Ella <ella@huggingface.co>
2026-02-19 18:38:37 +0100

    update setup

--- setup.py
+++ setup.py
@@ -28,8 +28,8 @@ except Exception as error:
 
 INSTALL_REQUIRE = [
     "torch>=2.1",
-    "optimum-onnx@git+https://github.com/huggingface/optimum-onnx.git@xadupre/transformers5",
-    "transformers>=4.45,<5.1",
+    "optimum-onnx@git+https://github.com/huggingface/optimum-onnx.git@transformers5",
+    "transformers>=4.45,<5.3",
     "setuptools",
     "huggingface-hub>=0.23.2,<2.0",
     "nncf>=2.19.0",
