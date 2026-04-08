ba3e7f9129102f2e0f79ae7f7c50530eb2149eb2
“Ella <ella@huggingface.co>
2026-04-08 16:21:56 +0200

    updated expected test quant num nodes given nncf latest release

--- tests/openvino/test_quantization.py
+++ tests/openvino/test_quantization.py
@@ -612,7 +612,7 @@ class OVWeightCompressionTest(unittest.TestCase):
             "gpt2",
             False,
             dict(bits=4, dtype="mxfp4", group_size=32),
-            {"model": {"int8": 4, "f4e2m1": 20, "f8e8m0": 20}},
+            {"model": {"f4e2m1": 20, "f8e8m0": 22}},
         ),
         (
             OVModelForCausalLM,
