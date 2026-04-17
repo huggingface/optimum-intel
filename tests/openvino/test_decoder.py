30e4fa5fda3241b401f4326ed38a6397c5cfef9a
Anastasiia Pnevskaia <anastasiia.pnevskaia@intel.com>
2026-04-17 15:09:33 +0200

    Update tests/openvino/test_decoder.py
    Co-authored-by: Shagun Gupta <137595846+MissLostCodes@users.noreply.github.com>

--- tests/openvino/test_decoder.py
+++ tests/openvino/test_decoder.py
@@ -313,7 +313,7 @@ class OVModelForCausalLMIntegrationTest(unittest.TestCase):
             supported_architectures -= {"bitnet"}
         if is_transformers_version("<", str(LFM2OpenVINOConfig.MIN_TRANSFORMERS_VERSION)):
             supported_architectures -= {"lfm2"}
-              if is_transformers_version("<",  str(LFM2MoeOpenVINOConfig.MIN_TRANSFORMERS_VERSION)):
+        if is_transformers_version("<",  str(LFM2MoeOpenVINOConfig.MIN_TRANSFORMERS_VERSION)):
             supported_architectures -= {"lfm2_moe"}
         # qwen3_vl_text a part of qwen3_vl architecture and is tested in seq2seq group
         if is_transformers_version(">=", str(Qwen3VLOpenVINOConfig.MIN_TRANSFORMERS_VERSION)):
