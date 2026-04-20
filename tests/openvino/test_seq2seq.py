ff1625a8697134e356b375f4313f76fd2c6040fe
Kazantsev, Roman <roman.kazantsev@intel.com>
2026-04-20 18:03:19 +0000

    Skip seq2seq tests

--- tests/openvino/test_seq2seq.py
+++ tests/openvino/test_seq2seq.py
@@ -683,6 +683,9 @@ class OVModelForVisualCausalLMIntegrationTest(OVSeq2SeqTestMixin):
 
     @parameterized.expand(SUPPORTED_ARCHITECTURES)
     def test_compare_to_transformers(self, model_arch):
+        if model_arch in ("llama4", "minicpmv") and is_openvino_version(">=", "2026.1.0"):
+            self.skipTest("CVS-183350: OpenVINO 2026.1.0 inference results mismatch")
+
         def compare_outputs(inputs, ov_model, transformers_model, generation_config):
             transformers_inputs = copy.deepcopy(inputs)
             ov_outputs = ov_model.generate(**inputs, generation_config=generation_config)
