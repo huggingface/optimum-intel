a80912ea6aaf3daf40fa6481c93e504e371968c2
“Ella <ella@huggingface.co>
2026-04-27 18:09:52 +0200

    use sdpa_mask instead of sdpa_mask_without_vmap for transformers v5 or higher

--- optimum/exporters/openvino/model_patcher.py
+++ optimum/exporters/openvino/model_patcher.py
@@ -250,13 +250,15 @@ def patch_cos_sin_cached_fp32(model):
                     dtype=torch.float32,
                 )
 
-
 # Adapted from https://github.com/huggingface/transformers/blob/v4.53.0/src/transformers/masking_utils.py#L433
 # Specifically for OpenVINO, we use torch.finfo(torch.float16).min instead of torch.finfo(dtype).min
 def eager_mask_without_vmap(*args, **kwargs) -> Optional[torch.Tensor]:
     kwargs.pop("allow_is_causal_skip", None)
     dtype = kwargs.get("dtype", torch.float32)
-    mask = sdpa_mask_without_vmap(*args, allow_is_causal_skip=False, **kwargs)
+    if is_transformers_version(">=", "5"):
+        mask = sdpa_mask(*args, use_vmap=False, **kwargs)
+    else:
+        mask = sdpa_mask_without_vmap(*args, allow_is_causal_skip=False, **kwargs)
     # we use torch.finfo(torch.float16).min instead torch.finfo(dtype).min to avoid an overflow but not
     # sure this is the right way to handle this, we are basically pretending that -65,504 is -inf
     mask = torch.where(
