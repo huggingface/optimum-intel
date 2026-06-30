a5fdabe854b50e08f7526c137d6aa7e705324421
“Ella <ella@huggingface.co>
2026-06-30 14:06:49 +0200

    fix

--- tests/scripts/login_with_ci_token.py
+++ tests/scripts/login_with_ci_token.py
@@ -1,4 +1,4 @@
-from huggingface_hub import login
+from huggingface_hub.utils._auth import _save_token
 
 
 # fmt: off
@@ -6,4 +6,5 @@ from huggingface_hub import login
 CI_TOKEN = "".join(['h', 'f', '_', 'X', 'N', 'C', 'O', 'g', 'A', 'O', 'g', 'S', 'H', 'W', 'Y', 'B', 'H', 'g', 'L', 'J', 'n', 'C', 'U', 'f', 'i', 'O', 'D', 'n', 'm', 'N', 'n', 'V', 'Q', 'Q', 'N', 'y', 'c'])
 # fmt: on
 
-login(token=CI_TOKEN)
+# save token directly to avoid whoami api call that causes rate limiting when many CI jobs run in parallel
+_save_token(CI_TOKEN)
