827ff6acdf22c0cfb3a40163ab42e4e9555b89ed
Kazantsev, Roman <roman.kazantsev@intel.com>
2026-07-15 10:47:53 +0000

    Apply formatting

--- tests/openvino/test_quantization.py
+++ tests/openvino/test_quantization.py
@@ -1433,7 +1433,9 @@ class OVWeightCompressionTest(unittest.TestCase):
 
     @parameterized.expand(
         SUPPORTED_ARCHITECTURES_WITH_AUTO_COMPRESSION,
-        name_func=lambda testcase_func, param_num, params: f"{testcase_func.__name__}_{parameterized.to_safe_name(params.args[1])}",
+        name_func=lambda testcase_func,
+        param_num,
+        params: f"{testcase_func.__name__}_{parameterized.to_safe_name(params.args[1])}",
     )
     def test_ovmodel_load_with_compressed_weights(self, model_cls, model_type, trust_remote_code):
         model = model_cls.from_pretrained(
@@ -1658,7 +1660,9 @@ class OVWeightCompressionTest(unittest.TestCase):
 
     @parameterized.expand(
         SUPPORTED_ARCHITECTURES_WITH_AUTO_COMPRESSION,
-        name_func=lambda testcase_func, param_num, params: f"{testcase_func.__name__}_{parameterized.to_safe_name(params.args[1])}",
+        name_func=lambda testcase_func,
+        param_num,
+        params: f"{testcase_func.__name__}_{parameterized.to_safe_name(params.args[1])}",
     )
     def test_ovmodel_load_with_uncompressed_weights(self, model_cls, model_type, trust_remote_code):
         model = model_cls.from_pretrained(
@@ -1791,7 +1795,9 @@ class OVWeightCompressionTest(unittest.TestCase):
         if is_transformers_version(">=", "5.5.0")
         else [],
         skip_on_empty=True,
-        name_func=lambda testcase_func, param_num, params: f"{testcase_func.__name__}_{parameterized.to_safe_name(params.args[0])}",
+        name_func=lambda testcase_func,
+        param_num,
+        params: f"{testcase_func.__name__}_{parameterized.to_safe_name(params.args[0])}",
     )
     def test_build_dataset(self, model_arch):
         model_id = MODEL_NAMES[model_arch]
