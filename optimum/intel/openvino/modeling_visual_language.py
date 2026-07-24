2d476cb9d28e8f7d7bca4e5319231d9f3f3c2ebe
Anastasiia Pnevskaia <anastasiia.pnevskaia@intel.com>
2026-07-24 11:32:11 +0200

    Add comment and check.

--- optimum/intel/openvino/modeling_visual_language.py
+++ optimum/intel/openvino/modeling_visual_language.py
@@ -5600,8 +5600,12 @@ class _OVGemma4UnifiedForCausalLM(_OVGemma3ForCausalLM):
             raise ValueError("Audio input is not supported")
         if getattr(tokenizer, "chat_template", None) is None:
             if image is not None:
+                # If chat template is not available we need to add image token manually, as there's a requirement
+                # that image token number should match the number ofprovided images:
+                # https://github.com/huggingface/transformers/blob/v5.10.0/src/transformers/models/gemma4_unified/processing_gemma4_unified.py#L187
                 image_token = getattr(processor, "image_token", "<|image|>")
-                text = f"{image_token}{text}"
+                if image_token not in text:
+                    text = f"{image_token}{text}"
             return processor(images=image, text=text, return_tensors="pt")
 
         conversation = [
